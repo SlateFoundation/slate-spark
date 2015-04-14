0148bda41a33278fbc9c9a268b96f9002289e10a
Jeffrey Mealo <jeffreymealo@gmail.com>
2015-04-14 12:43:03 -0400

    Generated Sencha app

---
+++ sencha-workspace/Spark2Manager/app/view/main/MainModel.js
@@ -0,0 +1,14 @@
+/**
+ * This class is the view model for the Main view of the application.
+ */
+Ext.define('Spark2Manager.view.main.MainModel', {
+    extend: 'Ext.app.ViewModel',
+
+    alias: 'viewmodel.main',
+
+    data: {
+        name: 'Spark2Manager'
+    }
+
+    //TODO - add data, formulas and/or methods to support your view
+});
