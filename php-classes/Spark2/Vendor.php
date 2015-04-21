814c2aa33247c8f57c95a953fe4711dcda1e7f53
Jeffrey Mealo <jeffreymealo@gmail.com>
2015-04-21 14:21:37 -0400

    Added Vendor,VendorDomain models and request handler

---
+++ php-classes/Spark2/Vendor.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Spark2;
+
+class Vendor extends \ActiveRecord
+{
+    public static $tableName = 's2_vendors';
+
+    public static $singularNoun = 'vendor';
+    public static $pluralNoun = 'vendors';
+
+    public static $collectionRoute = '/spark2/vendors';
+
+    public static $fields = [
+        'Name',
+        'Description' => [
+            'notnull' => false
+        ],
+        'LogoURL' => [
+            'notnull' => false
+        ]
+    ];
+
+    public static $relationships = [
+        'Domains' => [
+            'type' => 'one-many',
+            'class' => 'Spark2\VendorDomain',
+            'foreign' => 'VendorID'
+        ]
+    ];
+
+    public function getData() {
+        $data = parent::getData();
+        $data['Domains'] = $this->Domains;
+        return $data;
+    }
+}
---
+++ php-classes/Spark2/VendorsRequestHandler.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Spark2;
+
+class VendorsRequestHandler extends \RecordsRequestHandler
+{
+    public static $recordClass = Vendor::class;
+}
