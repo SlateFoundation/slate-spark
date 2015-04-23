9a5aa5a66c567aa82cf048e5915ab52f8c52bb92
Jeffrey Mealo <jeffreymealo@gmail.com>
2015-04-23 13:57:36 -0400

    Implement Tag and TagMaps request handlers and learn links

---
+++ php-classes/Spark2/LearnLink.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Spark2;
+
+class LearnLink extends \VersionedRecord
+{
+    public static $historyTable = 's2_history_learn_links';
+
+    public static $tableName = 's2_learn_links';
+
+    public static $singularNoun = 'learn link';
+    public static $pluralNoun = 'learn links';
+
+    public static $collectionRoute = '/spark2/learn-links';
+
+    public static $fields = [
+        'Title',
+        'URL',
+        'VendorID' => [
+            'type'    => 'uint',
+            'notnull' => false
+        ],
+        'DOK' => [
+            'type'    => 'tinyint',
+            'notnull' => false
+        ],
+        'Metadata' => [
+            'type'    => 'json',
+            'notnull' => false
+        ]
+    ];
+
+    public static $relationships = [
+        'Vendor' => [
+            'type' => 'one-one',
+            'class' => 'Spark2\Vendor',
+            'local' => 'VendorID'
+        ]
+    ];
+}
