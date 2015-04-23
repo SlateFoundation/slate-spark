61d958967c7aadbba08cde7469bca082a4e812dd
Jeffrey Mealo <jeffreymealo@gmail.com>
2015-04-23 13:56:28 -0400

    Implement ApplyLink, ApplyProject and ApplyTodo models, request handlers and stubs

---
+++ php-classes/Spark2/ApplyLinksRequestHandler.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Spark2;
+
+class ApplyLinksRequestHandler extends \RecordsRequestHandler
+{
+    public static $recordClass = ApplyLink::class;
+}
---
+++ php-classes/Spark2/ApplyToDosRequestHandler.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Spark2;
+
+class ApplyToDosRequestHandler extends \RecordsRequestHandler
+{
+    public static $recordClass = ApplyToDos::class;
+}
