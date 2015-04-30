61a8c3d0addfaa0070482c1d9f76207711b82184
Jeffrey Mealo <jeffreymealo@gmail.com>
2015-04-30 06:49:17 -0400

    Added apply controller and link model

---
+++ sencha-workspace/Spark2Manager/app/model/Link.js
@@ -0,0 +1,60 @@
+/*jslint browser: true, undef: true *//*global Ext*/
+
+Ext.define('Spark2Manager.model.Link', {
+    extend: 'Ext.data.Model',
+    requires: [
+        'Emergence.ext.proxy.Records',
+        'Ext.data.identifier.Negative'
+    ],
+
+    // model config
+    idProperty: 'ID',
+    identifier: 'negative',
+
+    fields: [
+        {
+            name: "ID",
+            type: "int",
+            useNull: true
+        },
+        {
+            name: "Class",
+            type: "string",
+            defaultValue: "Spark2\\Link"
+        },
+        {
+            name: "Created",
+            type: "date",
+            dateFormat: "timestamp",
+            useNull: true
+        },
+        {
+            name: "CreatorID",
+            type: "int",
+            useNull: true
+        },
+        {
+            name: "Title",
+            type: "string"
+        },
+        {
+            name: "URL",
+            type: "string"
+        },
+        {
+            name: "VendorID",
+            type: "int",
+            useNull: true
+        },
+        {
+            name: "Metadata",
+            type: "string",
+            useNull: true
+        }
+    ],
+
+    proxy: {
+        type: 'records',
+        url: '/spark2/links'
+    }
+});
