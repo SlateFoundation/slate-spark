6a50f7ff65cd146586c920153b564d1c5e59e2e7
Jeffrey Mealo <jeffreymealo@gmail.com>
2015-05-08 08:14:54 -0400

    Skeleton Assessment Container

--- sencha-workspace/Spark2Manager/app/view/assess/Panel.js
+++ sencha-workspace/Spark2Manager/app/view/assess/Panel.js
@@ -1,17 +1,80 @@
 Ext.define('Spark2Manager.view.assess.Panel', {
     requires: [
         'Ext.grid.plugin.CellEditing',
-        'Ext.selection.CellModel',
-        'Ext.form.field.Text',
-        'Ext.form.field.TextArea',
-        'Ext.button.Button'
+        'Ext.form.field.ComboBox',
+        'Ext.button.Button',
+        'Ext.form.field.Tag',
+        'Ext.toolbar.Paging'
     ],
 
-    store: [
-        'Assessments'
+    extend: 'Ext.grid.Panel',
+
+    xtype: 's2m-assess-panel',
+
+    store: 'Assessments',
+
+    bbar: [
+        { xtype: 'button', text: 'Add Assessment', action: 'create-assessment' }
     ],
 
-    extend: 'Ext.Container',
+    columns: [
+        {
+            text: 'Standard',
+            width: 250,
+            dataIndex: 'code',
+            editor: {
+                reference: 'standards',
+                xtype: 'tagfield',
+                store: 'StandardCodes',
+                queryMode: 'local',
+                displayField: 'code',
+                valueField: 'code',
+                allowBlank: false,
+                filterPickList: true,
+                multiSelect: true
+            }
+        },
+        {
+            text: 'Type',
+            dataIndex: 'AssessmentTypeID',
+            flex: 1,
+            editor: {
+                xtype: 'combobox',
+                store: 'AssessmentTypes',
+                queryMode: 'local',
+                displayField: 'Name',
+                valueField: 'ID',
+                grow: true,
+                editable: false,
+                allowBlank: false,
+                emptyText: '',
+                value: ''
+            },
+            renderer: function(val, col, record) {
+                // HELP: @themightychris: is there an easier way to do this sort of thing?
+                var assessmentType = Ext.getStore('AssessmentTypes').getById(val);
 
-    xtype: 's2m-assess-panel'
+                if (assessmentType) {
+                    return assessmentType.get('Name');
+                }
+
+                return '';
+            }
+        }
+    ],
+
+    selModel: 'cellmodel',
+
+    plugins: {
+        ptype: 'cellediting',
+        pluginId: 'cellediting',
+        clicksToEdit: 1
+    },
+
+    dockedItems: [{
+        xtype: 'pagingtoolbar',
+        store: 'Assessments',
+        dock: 'bottom',
+        displayInfo: true
+    }]
 });
