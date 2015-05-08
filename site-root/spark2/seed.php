d4691fe4373ce624613477faf594899d4ffa892a
Jeffrey Mealo <jeffreymealo@gmail.com>
2015-05-08 08:06:27 -0400

    Populate assessment types and LearnZillion "learns" in seed.php

--- site-root/spark2/seed.php
+++ site-root/spark2/seed.php
@@ -2,6 +2,25 @@
 
 namespace Spark2;
 
+function populateAssessmentTypes() {
+    $assessmentTypes = [
+        'Digital' => '0101110101010101010101010101',
+        'Teacher Directed' => 'COMING THIS FALL. DIRECTED BY A TEACHER. STARRING ROB SNYDER.',
+        'Dance off' => 'Show us your moves!'
+    ];
+
+    print 'Populating assessment types<br>';
+
+    foreach ($assessmentTypes as $assessmentType => $description) {
+        if(!AssessmentType::getByField('Name', $assessmentType)) {
+            $AssessmentType = new AssessmentType();
+            $AssessmentType->setField('Name', $assessmentType);
+            $AssessmentType->setField('Description', $description);
+            $AssessmentType->save();
+        }
+    }
+}
+
 function populateVendors() {
     $vendors = [
         [
@@ -19,6 +38,11 @@ function populateVendors() {
             'LogoURL'     => 'https://www.learnzillion.com/favicon.ico',
             'Description' => 'See visual, conceptual explanations of the Common Core State Standards. 4000+ videos tailored for grades 2-12.'
         ],
+        [
+            'Name'        => 'Illuminate Education',
+            'LogoURL'     => 'https://www.illuminateed.com/favicon.ico',
+            'Description' => 'We provide one groundbreaking system for all your K-12 data needs.'
+        ]
     ];
 
     $vendorDomains = [
@@ -43,6 +67,13 @@ function populateVendors() {
                 'Domain' => 'learnzillion.com',
                 'ContextClass' => LearnLink::class
             ]
+        ],
+        'Illuminate Education' => [
+            [
+                'Domain' => 'illuminateed.com',
+                'ContextClass' => Link::class /* TODO: If we use a generic link class we can't do different
+                                                 vendors by ContextClass */
+            ]
         ]
     ];
 
@@ -66,4 +97,49 @@ function populateVendors() {
     }
 }
 
+function populateLearnZillionLearns($tsvUrl = 'https://gist.githubusercontent.com/jmealo/511295bc5ec54cff4634/raw/0d0307e5892c0d068e40774a8e3b45331abc563f/04.13.2015-learnzillion-lesson-urls.tsv') {
+    print 'Populating LearnZillion Learns<br>';
+
+    $tsvFile = file($tsvUrl);
+    $tsvHeaders = str_getcsv(array_shift($tsvFile), "\t");
+    $learns = [];
+    $learnZillion = Vendor::getByField('Name', 'LearnZillion');
+    $learnZillionVendorID = $learnZillion->ID;
+
+    foreach ($tsvFile as $line) {
+        $learn = array_combine($tsvHeaders,  str_getcsv($line, "\t"));
+        $learns[] = $learn;
+    }
+
+    print LearnLink::getCount(['VendorID' => $learnZillionVendorID]) . ' LearnZillion learns in system<br>';
+
+    if (count($learns) > LearnLink::getCount(['VendorID' => $learnZillionVendorID])) {
+
+        foreach ($learns as $learn) {
+            $gradeLevel = [];
+
+            $learnLink = new LearnLink();
+            if (preg_match("/CCSS.Math.Content.(\\d+).*.*.*/u", $learn['Standard code'], $gradeLevel)) {
+                $learnLink->setField('GradeLevel', $gradeLevel[1]);
+            } else if (preg_match("/CCSS.ELA-Literacy.\\w+.(\\d+)/u", $learn['Standard code'], $gradeLevel)) {
+                $learnLink->setField('GradeLevel', $gradeLevel[1]);
+            }
+
+            $learnLink->setField('Standards',  [['standardCode' => $learn['Standard code']]]);
+            $learnLink->setField('Title',      $learn['Lesson title']);
+            $learnLink->setField('URL',        $learn['Student page URL']);
+            $learnLink->setField('VendorID', $learnZillionVendorID);
+            $learnLink->save();
+        }
+
+        print count($learns) . ' learns populated<br>';
+    } else {
+        print 'Skipped... learns already loaded<br>';
+    }
+
+
+}
+
 populateVendors();
+populateAssessmentTypes();
+populateLearnZillionLearns();
