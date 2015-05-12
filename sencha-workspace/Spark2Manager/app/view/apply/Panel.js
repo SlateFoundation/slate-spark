e73e321ab30ffb6e701eef8b94030c3832df35ab
Jeffrey Mealo <jeffreymealo@gmail.com>
2015-05-12 13:14:33 -0400

    Implemented Apply Project Data Grid minus right-side "Project Details"

--- sencha-workspace/Spark2Manager/app/view/apply/Panel.js
+++ sencha-workspace/Spark2Manager/app/view/apply/Panel.js
@@ -8,12 +8,15 @@ Ext.define('Spark2Manager.view.apply.Panel', {
         'Ext.form.FieldSet',
         'Ext.layout.container.Column',
         'Ext.layout.container.Anchor',
+        'Ext.layout.container.Fit',
         'Ext.grid.plugin.RowEditing',
         'Ext.toolbar.Paging',
         'Ext.XTemplate',
         'Ext.toolbar.Toolbar',
         'Spark2Manager.Util',
-        'Ext.Array'
+        'Ext.Array',
+        'Ext.form.RadioGroup',
+        'Ext.form.field.Date'
     ],
 
     stores: [
@@ -22,95 +25,102 @@ Ext.define('Spark2Manager.view.apply.Panel', {
 
     extend: 'Ext.form.Panel',
 
-    xtype: 's2m-apply-panel',
-    layout: 'column',
+    xtype:  's2m-apply-panel',
 
-    bodyPadding: 5,
-
-    defaults: {
-        bodyPadding: 15
+    layout: {
+        type: 'hbox',
+        align: 'stretch'
     },
 
     fieldDefaults: {
         labelAlign: 'left',
         labelWidth: 90,
-        anchor: '100%'
+        anchor:     '100%'
     },
 
-    dockedItems: [{
-        xtype: 'pagingtoolbar',
-        store: 'ApplyProjects',
-        dock: 'bottom',
-        displayInfo: true
-    },
-        {
-            xtype: 'toolbar',
-            reference: 'toolbar',
-            items: [{
-                text: 'Add Project',
-                tooltip: 'Add a new project',
-                action: 'add'
-            }, '-', {
-                reference: 'alignButton',
-                text: 'Align to Standards',
-                tooltip: 'Align this link to multiple standards easily using the standards picker',
-                action: 'align',
-                disabled: true
-            }, '-', {
-                reference: 'removeButton',
-                text: 'Delete Project',
-                tooltip: 'Remove the selected project',
-                action: 'delete',
-                disabled: true
-            }]
-        }],
-
+    bodyPadding: 5,
 
     items: [{
-        columnWidth: 0.65,
-        xtype: 'gridpanel',
-        store: 'ApplyProjects',
-        columns: [{
-            text: 'Project Title',
-            flex: 1,
-            sortable: true,
+        flex: 2,
+        xtype:           'gridpanel',
+        modelValidation: false,
+        store:           'ApplyProjects',
+
+        dockedItems: [{
+            xtype:       'pagingtoolbar',
+            store:       'ApplyProjects',
+            dock:        'bottom',
+            displayInfo: true
+        }, {
+            xtype:     'toolbar',
+            id: 'gridtoolbar',
+            items:     [{
+                text:    'Add Project',
+                tooltip: 'Add a new project',
+                action:  'add'
+            }, '-', {
+                text:      'Align to Standards',
+                tooltip:   'Align this link to multiple standards easily using the standards picker',
+                action:    'align',
+                disabled:  true
+            }, '-', {
+                text:      'Delete Project',
+                tooltip:   'Remove the selected project',
+                action:    'delete',
+                disabled:  true
+            }],
+            border: 5,
+            style: {
+                borderColor: 'red',
+                borderStyle: 'solid'
+            }
+        }],
+
+        columns:         [{
+            text:      'Project Title',
+            flex:      2,
+            sortable:  true,
             dataIndex: 'Title',
-            editor: {
-                xtype: 'textfield',
+            msgTarget: 'under',
+            editor:    {
+                xtype:      'textfield',
                 allowBlank: false
             }
         }, {
-            text: 'Description',
-            width: 75,
-            sortable: true,
-            dataIndex: 'Description',
-            editor: {
-                xtype: 'textarea',
-                allowBlank: false
+            text:      'Instructions',
+            flex:      3,
+            sortable:  true,
+            dataIndex: 'Instructions',
+            editor:    {
+                xtype:      'textarea',
+                allowBlank: false,
+                msgTarget:  'under'
             }
         }, {
-            text: 'Standards',
-            editor: {
-                xtype: 'tagfield',
+            text:      'Standards',
+            editor:    {
+                xtype:        'tagfield',
                 displayField: 'standardCode',
-                valueField: 'standardCode',
-                store: 'StandardCodes',
-                multiSelect: true,
+                valueField:   'standardCode',
+                store:        'StandardCodes',
+                multiSelect:  true,
                 getModelData: function () {
                     return {
                         'Standards': Ext.Array.map(this.valueStore.collect('standardCode'), function (code) {
-                            return {standardCode: code}
+                            return {
+                                standardCode: code
+                            }
                         })
                     };
                 },
-                listeners: {
+                listeners:    {
                     'autosize': function () {
                         // HACK: when the tagfield autosizes it pushes the update/cancel roweditor button down
                         this.up('roweditor').getFloatingButtons().setButtonPosition('bottom');
                     }
                 }
             },
-            renderer: function (val, col, record) {
+            renderer:  function (val, col, record) {
                 val = record.get('Standards');
 
                 if (!Array.isArray(val)) {
@@ -121,97 +131,58 @@ Ext.define('Spark2Manager.view.apply.Panel', {
                     return standard.standardCode || standard;
                 }).join(', ');
             },
-            width: 250,
-            dataIndex: 'Standards'
-        },
-            {
-                text: 'Grade',
-                dataIndex: 'GradeLevel',
-                width: 75,
-                editor: {
-                    xtype: 'combobox',
-                    store: ['PK', 'K', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
-                    editable: false,
-                    grow: true
-                }
-            },
-            {
-                text: 'DOK',
-                dataIndex: 'DOK',
-                editor: {
-                    xtype: 'combobox',
-                    store: [1,2,3,4]
-                }
-            },
-            {
-                text: 'Created By',
-                dataIndex: 'CreatorFullName'
-            },
-            {
-                xtype: 'datecolumn',
-                format:'m-d-Y',
-                text: 'Created',
-                dataIndex: 'Created'
+            dataIndex: 'Standards',
+            width: 250
+        }, {
+            text:      'Grade',
+            dataIndex: 'GradeLevel',
+            width:     60,
+            editor:    {
+                xtype:    'combobox',
+                store:    ['PK', 'K', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
+                editable: false,
+                grow:     true
             }
-        ],
-        listeners: {
+        }, {
+            text:      'DOK',
+            dataIndex: 'DOK',
+            editor:    {
+                xtype: 'combobox',
+                store: [1, 2, 3, 4]
+            },
+            width:     50
+        }],
+        listeners:       {
             selectionchange: function (model, records) {
-                var me = this.getView().up().up();
-
-                var rec = records[0];
+                var me = this,
+                    panel = me.findParentByType('s2m-apply-panel'),
+                    form = panel.getForm(),
+                    rec = records[0];
 
                 if (rec) {
-                    // TODO: @themightychris how can we get the scope of the assign panel better?
-                    me.getForm().loadRecord(rec);
-                    // me.getReferences().removeButton.setDisabled(selections.length === 0);
-                    // me.getReferences().alignButton.setDisabled(selections.length === 0);
+                    form.loadRecord(rec);
+                    me.down('#gridtoolbar button[action="delete"]').setDisabled(records.length === 0);
+                    me.down('#gridtoolbar button[action="align"]').setDisabled(records.length === 0);
                 }
             }
         },
 
         plugins: {
-            ptype: 'rowediting',
-            pluginId: 'rowediting',
-            clicksToEdit: 2
+            ptype:        'rowediting',
+            pluginId:     'rowediting',
+            clicksToEdit: 2,
+            errorSummary: false
         }
     }, {
-        columnWidth: 0.30,
-        margin: '0 0 0 10',
-        reference: 'form',
-        xtype: 'fieldset',
-        title: 'Project details',
-        layout: 'anchor',
-        defaultType: 'textfield',
+        flex: 1,
+        margin:      '0 0 0 10',
+        reference:   'form',
         items: [{
-            fieldLabel: 'Name',
-            name: 'name'
-        }, {
-            fieldLabel: 'Price',
-            name: 'price'
-        }, {
-            fieldLabel: '% Change',
-            name: 'pctChange'
-        }, {
-            xtype: 'datefield',
-            fieldLabel: 'Last Updated',
-            name: 'lastChange'
-        }, {
-            xtype: 'radiogroup',
-            fieldLabel: 'Rating',
-            columns: 3,
-            defaults: {
-                name: 'rating' //Each radio has the same name so the browser will make sure only one is checked at once
-            },
-            items: [{
-                inputValue: '0',
-                boxLabel: 'A'
-            }, {
-                inputValue: '1',
-                boxLabel: 'B'
-            }, {
-                inputValue: '2',
-                boxLabel: 'C'
-            }]
+            xtype:       'fieldset',
+            title:       'Project details',
+            layout:      'anchor',
+            defaultType: 'textfield',
+            items:       []
         }]
     }],
 
