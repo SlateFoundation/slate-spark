add2b65633de7110045042c869ec8ee7e1d06929
Jeffrey Mealo <jeffreymealo@gmail.com>
2015-05-13 11:33:29 -0400

    Extend FieldReplicator plugin to have: getValues, setValues and RemoveClones

--- sencha-workspace/Spark2Manager/app/plugin/FieldReplicator.js
+++ sencha-workspace/Spark2Manager/app/plugin/FieldReplicator.js
@@ -13,16 +13,43 @@
  * </code></pre>
  */
 Ext.define('Spark2Manager.plugin.FieldReplicator', {
+    requires: [
+        'Ext.plugin.Abstract',
+        'Ext.Function'
+    ],
+
+    xtype: 's2m-fieldreplicator',
+
+    extend: 'Ext.plugin.Abstract',
+
     alias: 'plugin.fieldreplicator',
 
     init: function(field) {
+        var me = this;
+
         // Assign the field an id grouping it with fields cloned from it. If it already
         // has an id that means it is itself a clone.
         if (!field.replicatorId) {
             field.replicatorId = Ext.id();
         }
 
-        field.on('blur', this.onBlur, this);
+        if (!me.cloneField) {
+            me.cloneField = field;
+        }
+
+        field.on('blur', me.onBlur, this);
+    },
+
+    cloneField: null,
+
+    replicate: function (field) {
+        var ownerCt = field.ownerCt,
+            replicatorId = field.replicatorId,
+            clone = field.cloneConfig({replicatorId: replicatorId}),
+            idx = ownerCt.items.indexOf(field);
+
+        ownerCt.add(idx + 1, clone);
+        return clone;
     },
 
     onBlur: function(field) {
@@ -30,8 +57,7 @@ Ext.define('Spark2Manager.plugin.FieldReplicator', {
             replicatorId = field.replicatorId,
             isEmpty = Ext.isEmpty(field.getRawValue()),
             siblings = ownerCt.query('[replicatorId=' + replicatorId + ']'),
-            isLastInGroup = siblings[siblings.length - 1] === field,
-            clone, idx;
+            isLastInGroup = siblings[siblings.length - 1] === field;
 
         // If a field before the final one was blanked out, remove it
         if (isEmpty && !isLastInGroup) {
@@ -42,11 +68,56 @@ Ext.define('Spark2Manager.plugin.FieldReplicator', {
             if (field.onReplicate) {
                 field.onReplicate();
             }
-            clone = field.cloneConfig({replicatorId: replicatorId});
-            idx = ownerCt.items.indexOf(field);
-            ownerCt.add(idx + 1, clone);
-            clone.focus();
+
+            this.replicate(field);
+        }
+    },
+
+    getValues: function() {
+        var me = this,
+            ownerCt = me.cloneField.ownerCt,
+            values = [];
+
+        ownerCt.items.each(function(field) {
+            var val = field.getValue();
+
+            if (field.replicatorId && val) {
+                values.push(val);
+            }
+        });
+
+        me.hasChanged = false;
+
+        return values;
+    },
+
+    setValues: function(vals) {
+        var me = this,
+            ownerCt = me.cloneField.ownerCt;
+
+        me.removeClones();
+
+        vals.forEach(function(val) {
+            var clone = me.replicate(me.cloneField);
+            clone.setValue(val);
+        });
+
+        ownerCt.add(me.cloneField);
+    },
+
+    removeClones: function() {
+        var me = this,
+            cloneField = me.cloneField,
+            ownerCt;
+
+        if (cloneField) {
+            ownerCt = cloneField.ownerCt;
+
+            ownerCt.items.each(function(field) {
+                if (field.replicatorId && field != cloneField) {
+                    ownerCt.remove(field);
+                }
+            });
         }
     }
-
 });
