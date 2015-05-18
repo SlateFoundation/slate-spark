abdc854aa9944eec645f06d8efcffadfe422d3da
Jeffrey Mealo <jeffreymealo@gmail.com>
2015-05-17 23:21:37 -0400

    Add /creators action to return a distinct list of users who have contributed content (destined for shared record class)

--- php-classes/Spark2/LearnLinksRequestHandler.php
+++ php-classes/Spark2/LearnLinksRequestHandler.php
@@ -5,4 +5,30 @@ namespace Spark2;
 class LearnLinksRequestHandler extends \RecordsRequestHandler
 {
     public static $recordClass = LearnLink::class;
+
+    public static function handleRecordsRequest($action = false)
+    {
+        switch ($action ? $action : $action = static::shiftPath()) {
+            case 'creators':
+                return static::handleCreatorsRequest();
+            default:
+                return parent::handleRecordsRequest($action);
+        }
+    }
+
+    public static function handleCreatorsRequest()
+    {
+        $recordClass = self::$recordClass;
+
+        $creators = \DB::allRecords(sprintf('
+        SELECT DISTINCT r.CreatorID,
+                        CONCAT(p.FirstName, " ", p.LastName) as CreatorFullName
+                   FROM %s r
+                   JOIN people p ON p.ID = r.CreatorID
+               ORDER BY p.LastName', $recordClass::$tableName));
+
+        return static::respondJson('creators', array(
+            'data' => $creators
+        ));
+    }
 }
