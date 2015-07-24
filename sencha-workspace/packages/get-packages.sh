a518c3e50f3033a51b524cc89cd7789a84dd6fed
Chris Alfano <chris@jarv.us>
2015-07-24 12:04:52 -0400

    removed MatchboxStudent app, upgraded workspace, generated SparkClassroomStudent app, imported slate-frame

--- sencha-workspace/packages/get-packages.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# framework hotfixes
-git clone -b ext/5/1/1/451 https://github.com/JarvusInnovations/sencha-hotfixes.git jarvus-hotfixes
-
-# feature packages
-git clone -b ext/5/1/1/451 https://github.com/JarvusInnovations/jarvus-ext-routing.git
