937e6cc2b0d49d5f9423094a1c8b8cf304e3d744
Jeffrey Mealo <jeffreymealo@gmail.com>
2015-07-24 17:13:59 -0400

    add dummy text to dummy fields

--- sencha-workspace/SparkRepositoryManager/app/view/sparkpoints/sparkpoint/Form.js
+++ sencha-workspace/SparkRepositoryManager/app/view/sparkpoints/sparkpoint/Form.js
@@ -5,21 +5,25 @@ Ext.define('SparkRepositoryManager.view.sparkpoints.sparkpoint.Form', {
     items: [{
         xtype: 'textfield',
         name: 'Title',
-        fieldLabel: 'Title'
+        fieldLabel: 'Title',
+        emptyText: 'Count really huge numbers'
     },{
         xtype: 'combobox',
         forceSelection: true,
         editable: false,
         triggerAction: 'all',
         store: [ 'K-2', '3-4', '5-6', '7-8', '9-12' ],
+        emptyText:'K-2',
         fieldLabel: 'Target Level Range'
     },{
         xtype: 'textfield',
         name: 'dummy',
+        emptyText: 'New Jersey',
         fieldLabel: 'Jurisdiction'
     },{
         xtype: 'textfield',
         name: 'dummy',
+        emptyText: 'Like a boat!',
         fieldLabel: 'Anchor'
     }]
 });
