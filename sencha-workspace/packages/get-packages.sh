840875dce685d9d771c2a303f897d55e00fea7fc
Chris Alfano <chris@jarv.us>
2015-07-24 14:18:34 -0400

    remove package scripts

--- sencha-workspace/packages/get-packages.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Self-destructing"
-echo "clone the repository with --recursive or run git submodule init && git submodule add"
-git submodule init
-git submodule update
-
-echo "get-packages.sh" >> .gitingore
-rm update-packages.sh
