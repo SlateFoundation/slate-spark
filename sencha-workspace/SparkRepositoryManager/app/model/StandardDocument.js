b93215e1fe496db486451c35e7f9d05ff15b507f
Bill Clinton <BillClinton@users.noreply.github.com>
2015-07-26 11:32:18 -0400

    documents tree updates.. still in development

--- sencha-workspace/SparkRepositoryManager/app/model/StandardDocument.js
+++ sencha-workspace/SparkRepositoryManager/app/model/StandardDocument.js
@@ -13,10 +13,16 @@ Ext.define('SparkRepositoryManager.model.StandardDocument', {
             name: 'id',
             type: 'string'
         },
-
         {
             name: 'title',
-            type: 'string'
+            type: 'string',
+            // TODO: this is still in development - bc
+            convert: function(val,rec) {
+                if (rec.get('document')) {
+                    return rec.get('document').title;
+                }
+                return val;
+            }
         }
     ],
 
--- sencha-workspace/SparkRepositoryManager/app/store/Jurisdictions.js
+++ sencha-workspace/SparkRepositoryManager/app/store/Jurisdictions.js
@@ -1,5 +1,5 @@
 /*jslint browser: true, undef: true *//*global Ext*/
-
+// TODO: change this file name to StandardDocuments and also needs further development- bc
 Ext.define('SparkRepositoryManager.store.Jurisdictions', {
     extend: 'Ext.data.TreeStore',
 
@@ -7,5 +7,19 @@ Ext.define('SparkRepositoryManager.store.Jurisdictions', {
         'SparkRepositoryManager.model.StandardDocument'
     ],
 
-    model: 'SparkRepositoryManager.model.StandardDocument'
+    model: 'SparkRepositoryManager.model.StandardDocument',
+
+    listeners: {
+        load: function(store, records, successful, operation, node, eOpts){
+            var i = 0;
+
+            if (node.id !== 'root') {
+                children = records[0].get('standardSets');
+                for (i; i<children.length; i++) {
+                    node.appendChild(Ext.apply(children[i],{leaf: true}),true);
+                }
+            }
+        }
+    }
+
 });
