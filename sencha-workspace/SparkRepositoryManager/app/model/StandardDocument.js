a03d4ab7c1bd72d0aad26667e255495122920bb7
Bill Clinton <BillClinton@users.noreply.github.com>
2015-07-25 22:07:31 -0400

    updated the class name of jurisdiction proxy/model

---
+++ sencha-workspace/SparkRepositoryManager/app/model/StandardDocument.js
@@ -0,0 +1,29 @@
+/*jslint browser: true, undef: true *//*global Ext*/
+Ext.define('SparkRepositoryManager.model.StandardDocument', {
+    extend: 'Ext.data.Model',
+    requires: [
+        'SparkRepositoryManager.proxy.StandardDocuments'
+    ],
+
+    // model config
+    idProperty: 'id',
+
+    fields: [
+        {
+            name: "id",
+            type: "string"
+        },
+
+        {
+            name: "title",
+            type: "string"
+        }
+    ],
+
+    proxy: {
+        type: 'spark-standarddocuments',
+        url: 'http://commonstandardsproject.com/api/v1/jurisdictions'
+    }
+});
+
+
