d8cd470575ce7337d9d0f6d7ed679185c379c17e
Chris Alfano <chris@jarv.us>
2015-07-26 19:13:25 -0400

    eliminated remaining height hacks

--- sencha-workspace/SparkRepositoryManager/app/widget/StandardField.js
+++ sencha-workspace/SparkRepositoryManager/app/widget/StandardField.js
@@ -37,23 +37,11 @@ Ext.define('SparkRepositoryManager.widget.StandardField', {
     listeners: {
         change: function (field, newHeight) {
             var me      = this,
-                ownerCt = me.ownerCt,
                 rowEditor = me.up('roweditor');
 
             if (rowEditor) {
                 /* HACK: while rowediting, when the tagfield autosizes it pushes the update/cancel roweditor buttons down */
                 rowEditor.getFloatingButtons().setButtonPosition('bottom');
-            } else if (me.up().$className === 'Ext.saki.grid.MultiSearch') {
-                /* HACK: in the filter/multisearch, when the tagfield autosizes, we must also autosize the filter row */
-                if (!me.autosized) {
-                    /* HACK: https://docs.m.sencha.com/forum/showthread.php?300648-Cannot-read-property-offsetHeight-of-undefined-in-BufferedRenderer-Broken-Ext-5.1&p=1098485&langid=4 */
-                    me.autosized = true;
-                    return;
-                }
-
-                if (ownerCt.height != newHeight) {
-                    ownerCt.setHeight(newHeight);
-                }
             }
         }
     },
