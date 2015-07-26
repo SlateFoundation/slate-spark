c98b3a5a46df0f5149b99a5ba56d079a518b4585
Chris Alfano <chris@jarv.us>
2015-07-25 23:08:04 -0400

    removed explicit requires for connection, apikit supports detection now

--- sencha-workspace/SparkRepositoryManager/app/proxy/API.js
+++ sencha-workspace/SparkRepositoryManager/app/proxy/API.js
@@ -2,9 +2,6 @@
 Ext.define('SparkRepositoryManager.proxy.API', {
     extend: 'Jarvus.proxy.API',
     alias: 'proxy.spark-api',
-    requires: [
-        'SparkRepositoryManager.API'
-    ],
 
     connection: 'SparkRepositoryManager.API'
 });
--- sencha-workspace/SparkRepositoryManager/app/proxy/Records.js
+++ sencha-workspace/SparkRepositoryManager/app/proxy/Records.js
@@ -2,9 +2,6 @@
 Ext.define('SparkRepositoryManager.proxy.Records', {
     extend: 'Emergence.proxy.Records',
     alias: 'proxy.spark-records',
-    requires: [
-        'SparkRepositoryManager.API'
-    ],
 
     connection: 'SparkRepositoryManager.API'
 });
