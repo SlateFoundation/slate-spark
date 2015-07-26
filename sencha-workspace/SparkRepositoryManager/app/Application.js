d3edbd0bb34de0d549c48eb02da0a90c6a6cfdf7
Chris Alfano <chris@jarv.us>
2015-07-26 00:26:09 -0400

    removed unused stores and models

--- sencha-workspace/SparkRepositoryManager/app/Application.js
+++ sencha-workspace/SparkRepositoryManager/app/Application.js
@@ -27,22 +27,6 @@ Ext.define('SparkRepositoryManager.Application', {
         'Sparkpoints'
     ],
 
-    // TODO: move all these to the controllers that actually use them
-    stores: [
-        'Assessments',
-        'Comments',
-        'GradeLevels',
-        'Links',
-        'Ratings',
-        'Standards',
-        'StandardMappings',
-        'StandardRefs',
-        'Tags',
-        'TagMaps',
-        'ConferenceResources',
-        'Jurisdictions'
-    ],
-
     init: function() {
         Ext.state.Manager.setProvider(Ext.create('Ext.state.LocalStorageProvider', {
             prefix: 'srm-'
