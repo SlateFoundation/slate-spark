e650d0e148b5b410d30e5ac742be7b3e61d08ed8
Bill Clinton <BillClinton@users.noreply.github.com>
2015-07-26 12:09:39 -0400

    converted "implements" tree to grid

--- sencha-workspace/SparkRepositoryManager/app/view/sparkpoints/sparkpoint/Implements.js
+++ sencha-workspace/SparkRepositoryManager/app/view/sparkpoints/sparkpoint/Implements.js
@@ -1,20 +1,19 @@
 Ext.define('SparkRepositoryManager.view.sparkpoints.sparkpoint.Implements', {
-    extend: 'Ext.tree.Panel',
+    extend: 'Ext.grid.Panel',
     xtype: 'srm-sparkpoints-sparkpointimplements',
 
     title: 'Implements',
 
     store:{
-        type: 'tree',
-        root: {
-            expanded: true,
-            children: [
-                { Title: 'K.CC.JK.1', leaf: true },
-                { Title: 'K.CC.JK.2', leaf: true },
-                { Title: 'K.CC.JK.3', leaf: true },
-                { Title: 'K.CC.JK.4', leaf: true }
-            ]
-        }
+        fields: [
+             {name: 'Title', type: 'string'}
+        ],
+        data: [
+            { Title: 'K.CC.JK.1' },
+            { Title: 'K.CC.JK.2' },
+            { Title: 'K.CC.JK.3' },
+            { Title: 'K.CC.JK.4' }
+        ]
     },
 
     rootVisible: false,
@@ -23,12 +22,13 @@ Ext.define('SparkRepositoryManager.view.sparkpoints.sparkpoint.Implements', {
     hideHeaders: true,
 
     columns: [{
-        xtype: 'treecolumn',
+        name: 'Title',
         flex: 5,
         dataIndex: 'Title'
     },{
         xtype: 'actioncolumn',
         width: 32,
+        // TODO: replace placeholder image
         icon: 'http://i.imgur.com/wb1NW8I.png'
     }],
 
