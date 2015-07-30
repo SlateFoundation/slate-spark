017e62c17100e97234efe3a700b67f61bbad5ca5
Bill Clinton <BillClinton@users.noreply.github.com>
2015-07-29 20:24:21 -0400

    added conditional to prevent error on null metaData. See inline note

--- sencha-workspace/SparkRepositoryManager/app/column/Sparkpoint.js
+++ sencha-workspace/SparkRepositoryManager/app/column/Sparkpoint.js
@@ -5,10 +5,17 @@ Ext.define('SparkRepositoryManager.column.Sparkpoint', {
     text: 'Sparkpoint',
     dataIndex: 'code',
     renderer: function(value, metaData, record) {
-        metaData.tdAttr += Ext.util.Format.attributes({
-            'data-qtitle': value,
-            'data-qtip': Ext.XTemplate.getTpl(record, 'tooltipTpl').apply(record.getData())
-        });
+        /*
+         * TODO: Note: I added the "if (metaData)" because adding nodes to tree store caused a "Cannot read property 'tdAttr' of null"
+         * error, but adding nodes manually may not be something we do as the app develops further.  Either way, it can't hurt.
+         * If we do add nodes with appendChild, we'll have to figure this out.
+         */
+        if (metaData) {
+            metaData.tdAttr += Ext.util.Format.attributes({
+                'data-qtitle': value,
+                'data-qtip': Ext.XTemplate.getTpl(record, 'tooltipTpl').apply(record.getData())
+            });
+        }
 
         return value;
     }
