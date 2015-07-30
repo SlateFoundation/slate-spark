7dd7ed2546742153b5027b98b51c7b35dc937d33
Bill Clinton <BillClinton@users.noreply.github.com>
2015-07-30 19:00:18 -0400

    finished form

--- sencha-workspace/SparkRepositoryManager/app/view/sparkpoints/sparkpoint/Form.js
+++ sencha-workspace/SparkRepositoryManager/app/view/sparkpoints/sparkpoint/Form.js
@@ -7,23 +7,17 @@ Ext.define('SparkRepositoryManager.view.sparkpoints.sparkpoint.Form', {
         'SparkRepositoryManager.field.LevelSlider'
     ],
 
+    defaults: {
+        anchor: '100%'
+    },
+
     items: [{
         xtype: 'textfield',
-        name: 'Title',
-        fieldLabel: 'Title',
-        emptyText: 'Count really huge numbers'
-    },{
-        xtype: 'combobox',
-        forceSelection: true,
-        editable: false,
-        triggerAction: 'all',
-        store: [ 'K-2', '3-4', '5-6', '7-8', '9-12' ],
-        emptyText:'K-2',
-        fieldLabel: 'Target Level Range'
+        fieldLabel: 'Code',
+        name: 'code'
     },{
         xtype: 'srm-field-levelslider',
-        fieldLabel: 'Target Level Range',
-        width: 320
+        fieldLabel: 'Target Level Range'
     },{
         xtype: 'checkboxfield',
         fieldLabel: 'Root standard',
@@ -35,14 +29,9 @@ Ext.define('SparkRepositoryManager.view.sparkpoints.sparkpoint.Form', {
         name: 'power',
         inputValue: '1'
     },{
-        xtype: 'textfield',
-        name: 'dummy',
-        emptyText: 'New Jersey',
-        fieldLabel: 'Jurisdiction'
-    },{
-        xtype: 'textfield',
-        name: 'dummy',
-        emptyText: 'Like a boat!',
-        fieldLabel: 'Anchor'
+        // TODO: Does a text area work here?
+        xtype: 'textarea',
+        fieldLabel: 'Editor\'s Memo',
+        name: 'note'
     }]
 });
