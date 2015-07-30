c5a8784a708c0d070821d53b49d6a8acbcca8514
Bill Clinton <BillClinton@users.noreply.github.com>
2015-07-30 03:13:03 -0400

    updated model, lowercased code field name, removed unused proxy

--- sencha-workspace/SparkRepositoryManager/app/store/sparkpoints/Sparkpoints.js
+++ sencha-workspace/SparkRepositoryManager/app/store/sparkpoints/Sparkpoints.js
@@ -3,27 +3,13 @@ Ext.define('SparkRepositoryManager.store.sparkpoints.Sparkpoints', {
     extend: 'Ext.data.Store',
     storeId: 'sparkpoints.Sparkpoints',
 
-    requires: [
-        'SparkRepositoryManager.model.Sparkpoint',
-        'SparkRepositoryManager.proxy.Records' // TODO: assuming we will need this when store is real, delete if not
-    ],
-
-    model: 'SparkRepositoryManager.model.Sparkpoint',
+    model: 'SparkRepositoryManager.model.sparkpoint.Sparkpoint',
 
     data: [
-        { ID: 1, Code: 'K.CC.1', Description: 'Count to 100 by ones and by tens.', M: 'K.CC.4a' },
-        { ID: 2, Code: 'K.CC.4a', Description: 'Understand how to stop counting', M: 'K.CC.3a' },
-        { ID: 3, Code: 'K.CC.4b', Description: 'Counting tiny numbers', M: 'K.G.1,2' },
-        { ID: 4, Code: 'K.G.1,2', Description: 'Counting big numbers', M: 'K.G.2,1' }
+        { ID: 1, code: 'K.CC.1', title: 'Count to 100 by ones and by tens.', M: 'K.CC.4a' },
+        { ID: 2, code: 'K.CC.4a', title: 'Understand how to stop counting', M: 'K.CC.3a' },
+        { ID: 3, code: 'K.CC.4b', title: 'Counting tiny numbers', M: 'K.G.1,2' },
+        { ID: 4, code: 'K.G.1,2', title: 'Counting big numbers', M: 'K.G.2,1' }
     ]
 
-/*
-    // TODO: make this store real
-    autoSync: true,
-
-    proxy: {
-        type: 'spark-records',
-        url: '/spark2/sparkpoints'
-    }
-*/
 });
