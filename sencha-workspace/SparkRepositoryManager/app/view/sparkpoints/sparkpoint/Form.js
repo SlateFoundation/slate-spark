c6a83032f1c3c61f8c38be6083d2b4463154b1c0
Bill Clinton <BillClinton@users.noreply.github.com>
2015-07-30 17:50:16 -0400

    added slider and checkboxes, still working on this

--- sencha-workspace/SparkRepositoryManager/app/view/sparkpoints/sparkpoint/Form.js
+++ sencha-workspace/SparkRepositoryManager/app/view/sparkpoints/sparkpoint/Form.js
@@ -2,6 +2,11 @@ Ext.define('SparkRepositoryManager.view.sparkpoints.sparkpoint.Form', {
     extend: 'Ext.form.Panel',
     xtype: 'srm-sparkpoints-sparkpointform',
 
+    requires: [
+        'Ext.form.field.Checkbox',
+        'SparkRepositoryManager.field.LevelSlider'
+    ],
+
     items: [{
         xtype: 'textfield',
         name: 'Title',
@@ -15,6 +20,20 @@ Ext.define('SparkRepositoryManager.view.sparkpoints.sparkpoint.Form', {
         store: [ 'K-2', '3-4', '5-6', '7-8', '9-12' ],
         emptyText:'K-2',
         fieldLabel: 'Target Level Range'
+    },{
+        xtype: 'srm-field-levelslider',
+        fieldLabel: 'Target Level Range',
+        width: 320
+    },{
+        xtype: 'checkboxfield',
+        fieldLabel: 'Root standard',
+        name: 'root',
+        inputValue: '1'
+    },{
+        xtype: 'checkboxfield',
+        fieldLabel: 'Power standard',
+        name: 'power',
+        inputValue: '1'
     },{
         xtype: 'textfield',
         name: 'dummy',
