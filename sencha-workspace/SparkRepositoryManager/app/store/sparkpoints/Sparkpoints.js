d8325d70875b2e5fc2f42a08ce5114054b4c6c0b
Chris Alfano <chris@jarv.us>
2015-08-08 22:27:25 -0400

    Do not set storeId on a store prototype

--- sencha-workspace/SparkRepositoryManager/app/store/sparkpoints/Sparkpoints.js
+++ sencha-workspace/SparkRepositoryManager/app/store/sparkpoints/Sparkpoints.js
@@ -1,7 +1,6 @@
 /*jslint browser: true, undef: true *//*global Ext*/
 Ext.define('SparkRepositoryManager.store.sparkpoints.Sparkpoints', {
     extend: 'Ext.data.Store',
-    storeId: 'sparkpoints.Sparkpoints',
 
     model: 'SparkRepositoryManager.model.Sparkpoint',
     remoteFilter: true,
