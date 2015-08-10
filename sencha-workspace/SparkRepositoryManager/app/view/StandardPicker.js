2af3ec0bde65563f53304902f68249263282dce7
Chris Alfano <chris@jarv.us>
2015-08-10 19:53:20 -0400

    Fix layout and scrolling performance on standard picker window

--- sencha-workspace/SparkRepositoryManager/app/view/StandardPicker.js
+++ sencha-workspace/SparkRepositoryManager/app/view/StandardPicker.js
@@ -22,53 +22,60 @@ Ext.define('SparkRepositoryManager.view.StandardPicker', {
 
     height: 600,
     width: 1024,
-    autoScroll: true,
     bodyPadding: 10,
     constrain: true,
     closable: true,
     modal: true,
 
-    items: [{
-        xtype: 'spark-standardfield',
-        fieldLabel: 'Standards Selected',
-        reference: 'tagfield',
-        grow: true,
-
-        listeners: {
-            beforeSelect: 'onBeforeTagFieldSelect',
-            beforedeselect: 'onBeforeTagFieldDeselect',
-            // HACK: beforedeselect doesn't fire unless the tagfield has focus, this makes removing tag items work
-            // reliably
-            render: function(tagfield) {
-                tagfield.getEl().on('mouseup', function(ev, el) {
-                    var previousSibling;
-
-                    if (el.classList.contains('x-tagfield-item-close')) {
-                        previousSibling = Ext.getStore('StandardsTree').findRecord('standardCode', el.previousSibling.textContent);
-
-                        if(previousSibling) {
-                            if (typeof previousSibling.get('checked') == 'boolean') {
-                                previousSibling.set('checked', false);
-                            }
-                        }
-                    }
-                });
-            }
-        }
+    layout: {
+        type: 'vbox',
+        align: 'stretch'
     },
 
-    {
-        alias: 's2m-standards-picker-panel',
-        reference: 'tree',
-        displayField: 'name',
-        xclass: 'Ext.tree.Panel',
-        store: 'StandardsTree',
-        rootVisible: false,
+    items: [
+        {
+            reference: 'tagfield',
+            xtype: 'spark-standardfield',
+            fieldLabel: 'Standards Selected',
+            grow: true,
+            stacked: false,
+    
+            listeners: {
+                beforeSelect: 'onBeforeTagFieldSelect',
+                beforedeselect: 'onBeforeTagFieldDeselect',
+                // HACK: beforedeselect doesn't fire unless the tagfield has focus, this makes removing tag items work
+                // reliably
+                render: function(tagfield) {
+                    tagfield.getEl().on('mouseup', function(ev, el) {
+                        var previousSibling;
+    
+                        if (el.classList.contains('x-tagfield-item-close')) {
+                            previousSibling = Ext.getStore('StandardsTree').findRecord('standardCode', el.previousSibling.textContent);
+    
+                            if(previousSibling) {
+                                if (typeof previousSibling.get('checked') == 'boolean') {
+                                    previousSibling.set('checked', false);
+                                }
+                            }
+                        }
+                    });
+                }
+            }
+        },
+        {
+            reference: 'tree',
+            flex: 1,
 
-        listeners: {
-            checkchange: 'onStandardsCheck'
+            xtype: 'treepanel',
+            displayField: 'name',
+            store: 'StandardsTree',
+            rootVisible: false,
+    
+            listeners: {
+                checkchange: 'onStandardsCheck'
+            }
         }
-    }],
+    ],
 
     bbar: [{
         text: 'Align Standards',
