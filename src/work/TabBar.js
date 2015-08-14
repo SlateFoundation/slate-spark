da63a86c0c750908ee5a4ef80828d85589523f9c
Ryon Coleman <ryon@jarv.us>
2015-08-14 16:53:30 -0400

    Add `section` tags back to tabs for controller

--- src/work/TabBar.js
+++ src/work/TabBar.js
@@ -13,16 +13,19 @@ Ext.define('SparkClassroom.work.TabBar', {
             {
                 // TODO: dynamic timers, and left icons
                 title: 'Learn &amp; Practice <div class="spark-tab-timer">1h</div>',
-                active: true
+                section: 'learn'
             },
             {
-                title: 'Conference <div class="spark-tab-timer">1h</div>'
+                title: 'Conference <div class="spark-tab-timer">1h</div>',
+                section: 'conference'
             },
             {
-                title: 'Apply <div class="spark-tab-timer">1h</div>'
+                title: 'Apply <div class="spark-tab-timer">1h</div>',
+                section: 'apply'
             },
             {
-                title: 'Assess <div class="spark-tab-timer">1h</div>'
+                title: 'Assess <div class="spark-tab-timer">1h</div>',
+                section: 'assess'
             }
         ]
     }
