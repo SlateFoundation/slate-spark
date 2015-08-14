55bfe035c863d6147790507b77416351705dfcc1
Ryon Coleman <ryon@jarv.us>
2015-08-13 21:36:22 -0400

    Remove hardcoded progress banner HTML

--- sencha-workspace/SparkClassroomStudent/app/view/work/learn/Main.js
+++ sencha-workspace/SparkClassroomStudent/app/view/work/learn/Main.js
@@ -3,16 +3,7 @@ Ext.define('SparkClassroomStudent.view.work.learn.Main', {
     extend: 'SparkClassroom.work.learn.Main',
     xtype: 'spark-student-work-learn',
 
-    config: {
-        layout: 'hbox'
-    },
-    
     initialize: function () {
         this.callParent(arguments);
-        
-        this.getComponent('standardGrid').insert(0, {
-            xtype: 'component',
-            html: 'Alexandra W has 3 out of 5 stars'
-        });
     }
 });
