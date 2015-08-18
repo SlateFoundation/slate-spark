2572bff0575121ed3d862741ecb2d13c9ab71446
Ryon Coleman <ryon@jarv.us>
2015-08-17 21:49:41 -0400

    Standardize work lists to a template-based component + add styling

--- src/work/conference/Main.js
+++ src/work/conference/Main.js
@@ -3,6 +3,7 @@ Ext.define('SparkClassroom.work.conference.Main', {
     extend: 'Ext.Container',
     xtype: 'spark-work-conference',
     requires: [
+        'SparkClassroom.work.conference.WorkList',
         'SparkClassroom.widget.SimpleHeading',
         'Jarvus.layout.Accordion'
     ],
@@ -23,83 +24,28 @@ Ext.define('SparkClassroom.work.conference.Main', {
                                 title: 'Standard 1 - CCSS.ELA.3.CC.4.A',
                                 items: [
                                     {
-                                        xtype: 'spark-simpleheading',
-                                        level: 2,
-                                        html: 'Guiding Questions'
-                                    },
-                                    {
-                                        // TODO would be more accessible and standards-friendly to use <ol> for the dataview and <li> for the item tags
-                                        xtype: 'dataview',
-                                        cls: 'spark-work-list',
-                                        store: {
-                                            fields: [
-                                                'index',
-                                                'text',
-                                                'type'
+                                        xtype: 'spark-worklist',
+                                        data: {
+                                            title: 'Guiding Questions',
+                                            items: [
+                                                { text: 'Example <foo> of a first guiding question.' },
+                                                { text: 'Example of a second guiding question that a student should be prepared to respond to.' },
+                                                { text: 'Example of a third guiding question that a student should be prepared to respond to.', studentSubmitted: true },
+                                                { text: '<div class="inline-flex-fullwidth-ct"><input placeholder="Add a guiding question you want to discuss with the teacher (optional)" class="flex-1"> <button type="submit">Add</button></div>', skipHtmlEncode: true }
                                             ],
-                                            data: [
-                                                { index: 1, text: 'Example of a first guiding question.' },
-                                                { index: 2, text: 'Example of a second guiding question that a student should be prepared to respond to.' },
-                                                { index: 3, text: 'Example of a third guiding question that a student should be prepared to respond to.', type: 'grad' },
-                                                // TODO text field and button
-                                                { index: 4, text: '[Add a guiding question you want to discuss with the teacher (optional)] [Add]'}
-                                            ]
-                                        },
-                                        itemCls: 'spark-work-list-item',
-                                        itemTpl: '<span class="item-index">{index}</span> <span class="item-text">{text}</span>'
+                                        }
                                     },
                                     {
-                                        xtype: 'spark-simpleheading',
-                                        level: 2,
-                                        html: 'Resources'
-                                    },
-                                    {
-                                        xtype: 'dataview',
-                                        cls: 'spark-work-list',
-                                        store: {
-                                            fields: [
-                                                'index',
-                                                'text',
-                                                'linkTitle',
-                                                'linkUrl'
-                                            ],
-                                            data: [
-                                                { index: 1, text: 'Title of resource', linkTitle: 'documenttoshare.pdf', linkUrl: '#' },
-                                                { index: 2, text: 'Title of resource', linkTitle: 'http://example.com', linkUrl: 'http://example.com' }
+                                        xtype: 'spark-worklist',
+                                        data: {
+                                            title: 'Resources',
+                                            items: [
+                                                { text: 'Title of resource', linkTitle: 'documenttoshare.pdf', linkUrl: '#' },
+                                                { text: 'Title of resource', linkUrl: 'http://example.com' }
                                             ]
-                                        },
-                                        itemCls: 'spark-work-list-item',
-                                        itemTpl: '<span class="item-index">{index}</span> <span class="item-text">{text}</span> &mdash; <a href="{linkUrl}">{linkTitle}</a>'
+                                        }
                                     }
                                 ],
-/*
-                                html: [
-                                    '<h5>Guiding Questions</h5>',
-                                    '<ol>',
-                                        '<li>Example of a first guiding question that a student should me prepared to respond to.</li>',
-                                        '<li>Example of a second guiding question that a student should me prepared to respond to.</li>',
-                                        '<li>Example of a third guiding question that a student should me prepared to respond to.</li>',
-                                        //FIXME: Replace with custod add with textfield
-                                        '<li><input type="text"><span class="button">Add</span></li>',
-                                    '</ol>',
-                                    '<hr>',
-                                    '<h5>Resources</h5>',
-                                    '<ol>',
-                                        '<li>Title of Resource - <a href="#">documenttoshare.pdf</a></li>',
-                                        '<li>Title of Resource - <a href="#">http://webpage.com</a></li>',
-                                    '</ol>'
-                                ].join('')
-*/
-                            },
-                            {
-                                xtype: 'component',
-                                html: 'stuff',
-                                title: 'Standard 2 - CCSS.ELA.3.CC.4.A'
-                            },
-                            {
-                                xtype: 'component',
-                                html: 'stuff',
-                                title: 'Standard 3 - CCSS.ELA.3.CC.4.A'
                             }
                         ]
                     }
---
+++ src/work/conference/WorkList.js
@@ -0,0 +1,26 @@
+Ext.define('SparkClassroom.work.conference.WorkList', {
+    extend: 'Ext.Component',
+    xtype: 'spark-worklist',
+    requires: [ 'Ext.XTemplate' ],
+
+    cls: 'spark-worklist-ct',
+
+    config: {
+        tpl: [
+            '<tpl if="title"><h2 class="spark-worklist-title">{title:htmlEncode}</h2></tpl>',
+            '<ol class="spark-worklist">',
+                '<tpl for="items">',
+                    '<li class="spark-worklist-item <tpl if="studentSubmitted">is-student-submitted</tpl>">',
+                        '<tpl if="skipHtmlEncode">{text}<tpl else>{text:htmlEncode}</tpl>',
+                        '<tpl if="linkUrl">',
+                            '&nbsp;&mdash; ',
+                            '<a href="{linkUrl}" target=_blank>',
+                                '<tpl if="linkTitle">{linkTitle}<tpl else>{linkUrl}</tpl>',
+                            '</a>',
+                        '</tpl>',
+                    '</li>',
+                '</tpl>',
+            '</ol>'
+        ]
+    }
+});
