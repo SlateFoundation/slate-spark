95aec3adde0efe0ee28610c3d4d1e018974a2ea2
Ryon Coleman <ryon@jarv.us>
2015-08-18 22:08:48 -0400

    Update view code and adopt grid for Apply tab (maybe to be replaced again later)

--- sencha-workspace/SparkClassroomStudent/app/view/work/apply/Main.js
+++ sencha-workspace/SparkClassroomStudent/app/view/work/apply/Main.js
@@ -2,102 +2,78 @@
 Ext.define('SparkClassroomStudent.view.work.apply.Main', {
     extend: 'Ext.Container',
     xtype: 'spark-student-work-apply',
+    requires: [
+        'SparkClassroom.widget.SimpleHeading'
+    ],
 
     config: {
-        layout: 'vbox',
         items: [
             {
-                xtype: 'toolbar',
-                items: [
+                xtype: 'spark-simpleheading',
+                level: 1,
+                cls: 'spark-view-headline',
+                html: 'Time to Apply your knowledge!'
+            },
+            {
+                xtype: 'grid',
+                titleBar: false,
+                store: {
+                    fields: [ 'title', 'description', 'standards', 'checked' ],
+                    data: [
+                        {
+                            title: 'Write It',
+                            description: 'Write a paragraph that has both active and passive voice sentences. Be sure to underline your sentences that display the learning target.',
+                            standards: [ '8.L.01b', '8.L.7', '8.L.5' ]
+                        },
+                        {
+                            title: 'Create It',
+                            description: 'Create a Powerpoint or Prezi to teach this learning target to your peers. Don’t limit yourself. Be creative and be sure to include practice activities. You may even decide to use Storybird or Storyboard.',
+                            standards: [ '8.L.7', '8.L.3' ],
+                            checked: true
+                        },
+                        {
+                            title: 'Wild Card',
+                            description: 'Record a video teaching students how to use active and passive voice. In your video, be sure to identify what is active and passive voice.'
+                        }
+                    ],
+                },
+                columns: [
                     {
-                        xtype: 'component',
-                        html: 'Suggested Applies',
-                        width: '80%'
+                        xtype: 'booleancolumn',
+                        dataIndex: 'checked',
+                        width: 100
                     },
                     {
-                        xtype: 'component',
-                        html: 'Standards Incorporated',
-                        width: '20%'
+                        xtype: 'templatecolumn',
+                        width: 700,
+                        cell: { encodeHtml: false },
+                        tpl: [
+                            '<h3 class="grid-item-title">{title}</h3>',
+                            '<tpl if="description"><div class="grid-item-detail">{description}</div></tpl>'
+                        ]
+                    },
+                    {
+                        xtype: 'templatecolumn',
+                        text: 'Standards Incorporated',
+                        dataIndex: 'standards',
+                        cell: { encodeHtml: false },
+                        tpl: '<tpl if="standards"><ul class="grid-token-list"><tpl for="standards"><li class="grid-token-item">{.}</li></tpl></ul></tpl>',
+                        width: '250'
                     }
-                ]
+                ],
+                height: 300
             },
             {
                 xtype: 'container',
-                layout: 'hbox',
+                layout: {
+                    type: 'hbox',
+                    pack: 'end'
+                },
                 items: [
                     {
-                        xtype: 'component',
-                        width: '80%',
-                        html: [
-                            '<input type="checkbox"><h5>Write it</h5>',
-                            '<span>Write A paragraph that has both active and passive voice sentences. Be sure to underline your sentences that display the learning target</span>'
-                        ].join('')
-                    },
-                    {
-                        xtype: 'component',
-                        width: '20%',
-                        html: [
-                            '<span>8.L.0.B</span> ',
-                            '<span>8.L.7.B</span> ',
-                            '<span>8.L.5.B</span> '
-                        ].join('')
-                    }
-                ]
-            },
-            {
-                xtype: 'container',
-                layout: 'hbox',
-                items: [
-                    {
-                        xtype: 'component',
-                        width: '80%',
-                        html: [
-                            '<input type="checkbox"><h5>Create it</h5>',
-                            '<span>Create a powerpoint or prezi to teach this learning target to your peers. Don\'t limit yourself. Be creative and be sure to include practice activities. You may even decide to use Storybird or Storeboard</span>'
-                        ].join('')
-                    },
-                    {
-                        xtype: 'component',
-                        width: '20%',
-                        html: [
-                            '<span>8.L.7</span> ',
-                            '<span>8.L.3</span> '
-                        ].join('')
-                    }
-                ]
-            },
-            {
-                xtype: 'container',
-                layout: 'hbox',
-                items: [
-                    {
-                        xtype: 'component',
-                        width: '80%',
-                        html: [
-                            '<input type="checkbox"><h5>Wild Card</h5>',
-                            '<span>Record a video on teaching students how to use active and passive voice. In your video, be sure to identify what is active and passive voice.</span>'
-                        ].join('')
-                    },
-                    {
-                        xtype: 'component',
-                        width: '20%',
-                        html: [
-                            'None'
-                        ].join('')
-                    }
-                ]
-            },
-            {
-                xtype: 'container',
-                layout: 'hbox',
-                items: [
-                    {
-                        xtype: 'component',
-                        flex: 1,
-                        html: [
-                            '<input type="checkbox"><h5>Suggest my own:</h5>',
-                            '<textarea></textarea>'
-                        ].join('')
+                        xtype: 'button',
+                        ui: 'action',
+                        text: 'Choose Selected Apply' // TODO polish this copy? include apply name?
                     }
                 ]
             }
