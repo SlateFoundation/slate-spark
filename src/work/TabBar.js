8eb997bca2e8649eadbe04176ded8f13fe37c44b
Chris Minniti <chrisminniti@Christophers-MacBook-Pro.local>
2015-08-27 09:35:38 -0400

    Add itemId to Work tabbar to support route rewrite logic in SparkClassroomTeacher

--- src/work/TabBar.js
+++ src/work/TabBar.js
@@ -14,19 +14,24 @@ Ext.define('SparkClassroom.work.TabBar', {
         items: [
             {
                 // TODO: dynamic timers, and left icons
+                // TODO: squash custom section after confirming it's not used anywhere in SparkClassroom
                 title: 'Learn &amp; Practice <div class="spark-tab-timer">1h</div>',
+                itemId: 'learn',
                 section: 'learn'
             },
             {
                 title: 'Conference <div class="spark-tab-timer">1h</div>',
+                itemId: 'conference',
                 section: 'conference'
             },
             {
                 title: 'Apply <div class="spark-tab-timer">1h</div>',
+                itemId: 'apply',
                 section: 'apply'
             },
             {
                 title: 'Assess <div class="spark-tab-timer">1h</div>',
+                itemId: 'assess',
                 section: 'assess'
             }
         ]
