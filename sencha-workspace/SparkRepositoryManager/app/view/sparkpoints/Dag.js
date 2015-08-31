13fec21faa0547ba4ae291da4dc16ed63e435d7c
Bill Clinton <BillClinton@users.noreply.github.com>
2015-08-31 17:39:56 -0400

    Add dynamic loading and click events

--- sencha-workspace/SparkRepositoryManager/app/view/sparkpoints/Dag.js
+++ sencha-workspace/SparkRepositoryManager/app/view/sparkpoints/Dag.js
@@ -8,6 +8,81 @@ Ext.define('SparkRepositoryManager.view.sparkpoints.Dag', {
         'Jarvus.draw.layout.BottomUp'
     ],
 
+    constructor: function(options){
+        Ext.Object.merge(this, options);
+        Ext.applyIf(this.settings.highlightedNodes, this.settings.nodes);
+        Ext.applyIf(this.settings.highlightedNodes.code, this.settings.nodes.code);
+        Ext.applyIf(this.settings.highlightedNodes.title, this.settings.nodes.title);
+        Ext.applyIf(this.settings.highlightedNodes.rect, this.settings.nodes.rect);
+        Ext.applyIf(this.settings.highlightedNodes.dot, this.settings.nodes.dot);
+        this.callParent();
+    },
+
+    loadSparkpoint: function(sparkpoint) {
+        var sparkpoint_edges,
+            dependencies = [],
+            dependents = [],
+            relation,
+            root,
+            nodes = {},
+            edges = [],
+            dag,
+            i = 0;
+
+        if (sparkpoint) {
+            sparkpoint_edges = sparkpoint.get('sparkpoints_edges');
+
+            for (i; i<sparkpoint_edges.length; i++) {
+                relation = sparkpoint_edges[i];
+                if (relation.rel_type==='dependency') {
+                    if (relation.id === relation.target_sparkpoint_id) {
+                        dependencies.push(relation);
+                    } else if (relation.id === relation.source_sparkpoint_id) {
+                        dependents.push(relation);
+                    }
+                }
+            }
+
+            root = dependencies.length +1;
+
+            for (i=1; i<=dependencies.length; i++) {
+                nodes[i] = dependencies[i-1];
+                edges.push([i,root]);
+            }
+
+            nodes[root] = Ext.apply(sparkpoint.getData(),{highlighted:1});
+
+            for (i=1; i<=dependents.length; i++) {
+                nodes[root+i] = dependents[i-1];
+                edges.push([root,root+i]);
+            }
+
+            dag = {
+                nodes: nodes,
+                edges: edges
+            };
+
+            this.refreshData(dag);
+
+        }
+
+    },
+
+    refreshData: function(dag) {
+        var layout = Ext.createByAlias('daglayout.' + this.computeLayout);
+
+        this.dagLayout = layout.fn(dag);
+
+        this.allShapes = this.applyLayout(this.dagLayout);
+
+        this.center();
+    },
+
+    eraseAll : function() {
+        this.getSurface().removeAll();
+        this.allShapes = null;
+    },
+
     /*
      * Method: buildNode
      *
@@ -20,7 +95,8 @@ Ext.define('SparkRepositoryManager.view.sparkpoints.Dag', {
      *
      */
     buildNode : function(surface, node, x, settings) {
-        var nodSettings = node.highlighted ? settings.highlightedNodes : settings.nodes,
+        var me = this,
+            nodSettings = node.highlighted ? settings.highlightedNodes : settings.nodes,
             rectWidth = nodSettings.rect.width,
             rectHeight = nodSettings.rect.height,
             dotCount = node.dependencies_count,
@@ -85,7 +161,7 @@ Ext.define('SparkRepositoryManager.view.sparkpoints.Dag', {
                 type: 'text',
                 x: x + nodSettings.title.marginWidth,
                 y: codeHeight+nodSettings.title.marginHeight,
-                text: this.wordwrap(node.teacher_title,50,"\n" )
+                text: me.wordwrap(node.teacher_title,50,"\n" )
             });
 
             title.setAttributes(nodSettings.title, true);
@@ -108,6 +184,14 @@ Ext.define('SparkRepositoryManager.view.sparkpoints.Dag', {
             rect.width = rectBB.width;
             rect.height = rectBB.height;
 
+            // create mask sprite for interactions
+            var mask = surface.add({
+                type: 'rect',
+                attr: rect.attr
+            });
+
+            mask.setAttributes({opacity: 0});
+
             for (i =0; i <= dotCount-1; i++) {
                 dotStart = x + nodSettings.dot.marginWidth + nodSettings.dot.radius;
                 dots[i] = surface.add({
@@ -125,17 +209,26 @@ Ext.define('SparkRepositoryManager.view.sparkpoints.Dag', {
 
             codeX = 2;
             codeY = codeBB.height/2;
-            this.TRANSLATE(sprites, codeX, codeY);
+            me.TRANSLATE(sprites, codeX, codeY);
 
             sprites.add(title);
             sprites.add(rect);
+            sprites.add(mask);
 
             for (i =0; i <= dotCount-1; i++) {
                 sprites.add(dots[i]);
             }
 
             // center the cell vertically
-            this.TRANSLATE(sprites, 0, (rectHeight-rectBB.height)/2);
+            me.TRANSLATE(sprites, 0, (rectHeight-rectBB.height)/2);
+
+            mask.on({
+                click: function() {
+                    if (!node.highlighted) {
+                        me.fireEvent('maskclick', node);
+                    }
+                }
+            });
         }
         sprites.redraw();
 
