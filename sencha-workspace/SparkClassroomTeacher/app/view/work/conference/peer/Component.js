9ee216914c0f8138d37bcd785b21beb411209c46
Ryon Coleman <ryon@jarv.us>
2015-08-31 22:32:15 -0400

    Update teacher conference tab to use data-driven question list

--- sencha-workspace/SparkClassroomTeacher/app/view/work/conference/peer/Component.js
+++ sencha-workspace/SparkClassroomTeacher/app/view/work/conference/peer/Component.js
@@ -1,27 +1,35 @@
 /*jslint browser: true, undef: true, laxcomma:true *//*global Ext*/
 Ext.define('SparkClassroomTeacher.view.work.conference.peer.Component', {
-    extend: 'Ext.Component',
+    extend: 'Ext.Container',
     xtype: 'spark-teacher-work-conference-peer-component',
+    cls: 'content-card',
 
     config: {
-        html: [
-            '<h5>Restate the Learn target in your own words</h5>',
-            '<span>Active text field with green line belowe. </span> <br><br>',
-            
-            '<h5>Describe the steps used to show understanding of the skill</h5>',
-            'This is how I would describe the skill.',
-            
-            '<h5>Cite 3 real world examples of the learning target</h5>',
-            '<ol>',
-                '<li>Real world example</li>',
-                '<li>Real world example</li>',
-                '<li>Real world example</li>',
-            '</ol>',
-            '<hr>',
-            
-            '<h4>Peer Conference:</h4> (optional)',
-            '<span>Peer: Peer Name</span>',
-            '<span> Feedback from Peer: Student\'s restatement</span>'
-        ].join('')
+        data: {
+            questions: [
+                {
+                    q: 'Restate the Learn target in your own words.',
+                    a: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+                },
+                {
+                    q: 'Describe the steps used to show understanding of the skill.',
+                    a: '<ol><li>First step</li><li>Second step</li><li>Third step</li></ol>'
+                },
+                {
+                    q: 'Cite three real-world examples of the learning target.',
+                    a: '<ol><li>First example</li><li>Second example</li><li>Third example</li></ol>'
+                }
+            ]
+        },
+        tpl: [
+            '<dl class="spark-qna">',
+                '<tpl foreach="questions">',
+                    '<div class="spark-qna-item">',
+                        '<dt class="spark-qna-q">{q}</dt>',
+                        '<dd class="spark-qna-a">{a}</dd>',
+                    '</div>',
+                '</tpl>',
+            '</dl>'
+        ]
     }
 });
