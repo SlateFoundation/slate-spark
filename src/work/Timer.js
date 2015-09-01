312e70b6edae3ed13d01a2571834ea3c3f0f5aa9
Ryon Coleman <ryon@jarv.us>
2015-08-31 22:23:30 -0400

    Add work timer component

---
+++ src/work/Timer.js
@@ -0,0 +1,15 @@
+Ext.define('SparkClassroom.work.Timer', {
+    extend: 'Ext.Component',
+    xtype: 'spark-work-timer',
+    cls: 'spark-work-timer',
+
+    config: {
+        data: {
+            time: '0:00'
+        },
+        tpl: [
+            '<div class="timer-icon"></div>',
+            '<div class="timer-counter">{time}</div>'
+        ]
+    },
+});
