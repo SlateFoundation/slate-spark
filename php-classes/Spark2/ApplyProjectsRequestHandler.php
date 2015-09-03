ae71fee39d12bf85c9de9b079f5e0a799e22bf4f
Chris Alfano <chris@jarv.us>
2015-09-03 19:03:34 +0000

    Permit browse requests from spark origins

--- php-classes/Spark2/ApplyProjectsRequestHandler.php
+++ php-classes/Spark2/ApplyProjectsRequestHandler.php
@@ -4,5 +4,15 @@ namespace Spark2;
 
 class ApplyProjectsRequestHandler extends SparkPointRequestHandler
 {
+    public static $anonymousOrigins = [];
+
     public static $recordClass = ApplyProject::class;
+
+    public static function checkBrowseAccess($browseArguments)
+    {
+        return (
+            in_array($_SERVER['HTTP_ORIGIN'], static::$anonymousOrigins) ||
+            parent::checkBrowseAccess($browseArguments)
+        );
+    }
 }
