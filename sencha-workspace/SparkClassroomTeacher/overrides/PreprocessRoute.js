b1f0a42f84fb855eb059d25492c123516de16256
Chris Alfano <chris@jarv.us>
2015-09-06 13:43:29 -0400

    Merge rewrite and beforeroute preprocessing

--- sencha-workspace/SparkClassroomTeacher/overrides/PreprocessRoute.js
+++ sencha-workspace/SparkClassroomTeacher/overrides/PreprocessRoute.js
@@ -4,15 +4,35 @@
 Ext.define('Jarvus.routing.PreprocessRoute', {
     override: 'Ext.app.route.Router',
 
-    onStateChange: function (token) {
+    onStateChange: function (originalToken) {
         var me = this,
             thisMethod = arguments.callee,
-            resume = function(newToken) {
-                thisMethod.$owner.prototype[thisMethod.$name].call(me, Ext.isEmpty(newToken, true) ? token : newToken);
+            resume = function(resumeToken) {
+                resumeToken = Ext.isEmpty(resumeToken, true) ? originalToken : resumeToken;
+
+                var recognized = me.recognize(resumeToken),
+                    route = recognized && recognized.route,
+                    controller = route && route.controller,
+                    rewrite = route && route.rewrite,
+                    rewriteToken;
+
+                if (rewrite) {
+                    if (Ext.isString(rewrite)) {
+                        rewrite = route.rewrite = controller[rewrite];
+                    }
+
+                    rewriteToken = rewrite.apply(controller, [resumeToken, recognized.args.args, route]);
+
+                    if (Ext.isEmpty(rewriteToken, true)) {
+                        rewriteToken = resumeToken;
+                    }
+                }
+
+                thisMethod.$owner.prototype[thisMethod.$name].call(me, rewriteToken);
             };
 
-        if (false !== me.application.fireEvent('beforeroute', token, resume)) {
-            me.callParent([token]);
+        if (false !== me.application.fireEvent('beforeroute', originalToken, resume)) {
+            resume(originalToken);
         }
     }
 });
