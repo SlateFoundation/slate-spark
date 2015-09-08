46a854c1c8d768d856564c19eaf02bb32d96f0c3
Chris Alfano <chris@jarv.us>
2015-09-08 11:08:36 -0400

    Write new section into route on select

--- sencha-workspace/SparkClassroomTeacher/app/controller/Viewport.js
+++ sencha-workspace/SparkClassroomTeacher/app/controller/Viewport.js
@@ -5,6 +5,12 @@ Ext.define('SparkClassroomTeacher.controller.Viewport', {
         'Ext.MessageBox'
     ],
 
+    tokenSectionRe: /^([^:]+):(.*)$/,
+
+    config: {
+        selectedSection: null
+    },
+
     views: [
         'TitleBar@SparkClassroom',
         'NavBar@SparkClassroom',
@@ -84,10 +90,6 @@ Ext.define('SparkClassroomTeacher.controller.Viewport', {
         }
     },
 
-    config: {
-        selectedSection: null
-    },
-
     onLaunch: function() {
         var me = this;
 
@@ -103,7 +105,13 @@ Ext.define('SparkClassroomTeacher.controller.Viewport', {
     },
 
     updateSelectedSection: function(section, oldSection) {
+        var token = Ext.util.History.getToken(),
+            sectionMatch = token && this.tokenSectionRe.exec(token);
+
         console.info('updateSelectedSection(%o, %o)', section, oldSection);
+
+        // redirect with the current un-prefixed route or an empty string to write the new section into the route
+        this.redirectTo((sectionMatch && sectionMatch[2]) || '');
     },
 
     onBeforeRedirect: function(token, resume) {
@@ -119,15 +127,14 @@ Ext.define('SparkClassroomTeacher.controller.Viewport', {
     },
 
     onBeforeRoute: function(token, resume) {
-        var sectionMatch = token && token.match(/^([^:]+):(.+)?$/),
+        var me = this,
+            sectionMatch = token && me.tokenSectionRe.exec(token),
             sectionCode = sectionMatch && sectionMatch[1];
 
         console.info('onBeforeRoute(%s)', token);
 
         if (sectionCode) {
-            console.warn('TODO: load section %s and then resume path %s', sectionCode, sectionMatch[2]);
-
-            this.setSelectedSection(sectionCode);
+            me.setSelectedSection(sectionCode);
             console.info('->resume(%s)', sectionMatch[2]);
             resume(sectionMatch[2]);
             return false;
@@ -143,7 +150,7 @@ Ext.define('SparkClassroomTeacher.controller.Viewport', {
     },
 
     onSectionSelectChange: function(selectField, section, oldSection) {
-        this.redirectTo(section.get('Code'));
+        this.setSelectedSection(section.get('Code'));
         // var studentStore = Ext.getStore('Students'),
         //     sectionStore = Ext.getStore('SectionStudents'),
         //     classCode = newValue.get('Code'),
