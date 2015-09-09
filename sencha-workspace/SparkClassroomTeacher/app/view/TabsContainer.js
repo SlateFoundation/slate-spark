836fa8e016ba073e7d5bfa48c8ac9ba87b26df9c
Chris Alfano <chris@jarv.us>
2015-09-09 11:37:48 -0400

    Add TODO re: worktabs

--- sencha-workspace/SparkClassroomTeacher/app/view/TabsContainer.js
+++ sencha-workspace/SparkClassroomTeacher/app/view/TabsContainer.js
@@ -1,4 +1,7 @@
 /*jslint browser: true, undef: true, laxcomma:true *//*global Ext*/
+/**
+ * TODO: extend SparkClassroom.work.Main
+ */
 Ext.define('SparkClassroomTeacher.view.TabsContainer', {
     extend: 'Ext.Container',
     xtype: 'spark-teacher-tabscontainer',
