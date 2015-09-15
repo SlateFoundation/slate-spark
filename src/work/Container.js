5e07e56f3bd3c78b088cf0f9fa1092a117979437
Chris Alfano <chris@jarv.us>
2015-09-15 12:35:42 -0400

    Rename work.Main to work.Container

--- src/work/Container.js
+++ src/work/Container.js
@@ -1,17 +1,17 @@
 /*jslint browser: true, undef: true, laxcomma:true *//*global Ext*/
-Ext.define('SparkClassroom.work.Main', {
+Ext.define('SparkClassroom.work.Container', {
     extend: 'Ext.Container',
     requires: [
         'SparkClassroom.work.TabBar'
     ],
-    xtype: 'spark-work',
+    xtype: 'spark-work-ct',
 
     config: {
         layout: 'vbox',
         title: 'Student Work',
         items: [
             // TODO: add a config option to the Teacher version of this class that, if set to an array of students, causes
-            // this toolbar to be inserted 
+            // this toolbar to be inserted
             {
                 xtype: 'spark-work-tabbar',
                 docked: 'top'
