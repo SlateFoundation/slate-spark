5fa175b1dba0f45a6d5a95de0d3df47709979718
Chris Alfano <chris@jarv.us>
2015-09-15 12:30:28 -0400

    Move cls config to appropriate position and remove misplaced TODO

--- sencha-workspace/SparkClassroomTeacher/app/view/TabsContainer.js
+++ sencha-workspace/SparkClassroomTeacher/app/view/TabsContainer.js
@@ -1,17 +1,14 @@
 /*jslint browser: true, undef: true, laxcomma:true *//*global Ext*/
-/**
- * TODO: extend SparkClassroom.work.Main
- */
 Ext.define('SparkClassroomTeacher.view.TabsContainer', {
     extend: 'Ext.Container',
     xtype: 'spark-teacher-tabscontainer',
-    cls: 'spark-teacher-tabscontainer',
     requires: [
         'SparkClassroomTeacher.view.TabBar'
     ],
 
     config: {
         autoDestroy: false,
+        cls: 'spark-teacher-tabscontainer',
         items: [
             {
                 docked: 'top',
