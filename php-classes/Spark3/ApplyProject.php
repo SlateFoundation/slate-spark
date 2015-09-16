850c992fb4f572ae0b86b220f5fcb3ae2f62640a
Chris Alfano <chris@jarv.us>
2015-09-16 23:29:27 +0000

    Add mocks of new apply classes

---
+++ php-classes/Spark3/ApplyProject.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Spark3;
+
+class ApplyProject extends \Emergence\ActiveRecord\MysqlRecord
+{
+    public static $historyTable = 's2_history_apply_projects';
+
+    public static $tableName = 's2_apply_projects';
+
+    public static $singularNoun = 'apply project';
+    public static $pluralNoun = 'apply projects';
+
+    public static $collectionRoute = '/spark2/apply-projects';
+
+    public static $fields = [
+        'Title',
+        'Instructions' => 'clob',
+        'GradeLevel' => [
+            'type'    => 'enum',
+            'notnull' => false,
+            'values'  => ['PK', 'K', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+        ],
+        'DOK' => [
+            'type'    => 'tinyint',
+            'notnull' => false
+        ],
+        'Standards' => [
+            'type'    => 'json',
+            'notnull' => false
+        ],
+        'Todos' => [
+            'type'    => 'json',
+            'notnull' => false
+        ],
+        'Links' => [
+            'type'    => 'json',
+            'notnull' => false
+        ],
+        'TimeEstimate' => [
+            'type'    => 'smallint',
+            'notnull' => false
+        ],
+        'Metadata' => [
+            'type'    => 'json',
+            'notnull' => false
+        ]
+    ];
+
+    public static $searchConditions = [
+        'DOK' => [
+            'qualifiers' => ['DOK', 'dok'],
+            'sql' => 'DOK = "%s"'
+        ],
+
+        'Title' => [
+            'qualifiers' => ['title'],
+            'sql' => 'Title LIKE "%%%s%%"'
+        ]
+    ];
+}
---
+++ php-classes/Spark3/ApplyProjectsRequestHandler.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Spark3;
+
+class ApplyProjectsRequestHandler extends \Emergence\ActiveRecord\RequestHandler
+{
+    public static $anonymousOrigins = [];
+
+    public static $recordClass = ApplyProject::class;
+    public static $accountLevelRead = 'User';
+
+    public static function checkBrowseAccess($browseArguments)
+    {
+        return (
+            in_array($_SERVER['HTTP_ORIGIN'], static::$anonymousOrigins) ||
+            parent::checkBrowseAccess($browseArguments)
+        );
+    }
+
+    public static function checkReadAccess(\ActiveRecord $Record, $suppressLogin = false)
+    {
+        return (
+            in_array($_SERVER['HTTP_ORIGIN'], static::$anonymousOrigins) ||
+            parent::checkReadAccess($Record, $suppressLogin)
+        );
+    }
+}
