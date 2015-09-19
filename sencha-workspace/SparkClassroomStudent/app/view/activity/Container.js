66be25d089d5204e70fc14375fdf9da6de1010fe
Ryon Coleman <ryon@jarv.us>
2015-09-18 20:20:55 -0400

    Update student activity list template

--- sencha-workspace/SparkClassroomStudent/app/view/activity/Container.js
+++ sencha-workspace/SparkClassroomStudent/app/view/activity/Container.js
@@ -14,7 +14,12 @@ Ext.define('SparkClassroomStudent.view.activity.Container', {
     	width: 300,
         items: [
         	{
-        		xtype: 'spark-activity-list'
+        		xtype: 'spark-activity-list',
+        		itemTpl: [
+                    '<strong class="activity-verb">Completed L&P</strong> ',
+                    '<span class="activity-preposition">of</span> ',
+                    '<span class="activity-object activity-standard">CC.SS.Math.Content.1.OA.A.1</span>'
+        		]
         	}
         ]
     }
