2d39687a77199b76a216d1a8d07b7a5a5ee44261
Chris Alfano <chris@jarv.us>
2015-09-23 16:41:11 -0400

    Generalize alignment override in hotfix and remove from subpanel class

--- src/NavSubpanel.js
+++ src/NavSubpanel.js
@@ -7,17 +7,5 @@ Ext.define('SparkClassroom.NavSubpanel', {
         cls: 'spark-navsubpanel',
         width: 400,
         left: 0 // must be set here to force container to add as floating, see https://www.sencha.com/forum/showthread.php?300896
-    },
-
-    getAlignmentInfo: function(component, alignment) {
-        var scrollable = this.up('{getScrollable()}').getScrollable(),
-            alignmentInfo = this.callParent(arguments);
-
-        if (scrollable) {
-            // shift alignToBox.top by containter padding and scroll position
-            alignmentInfo.stats.alignToBox.top -= scrollable.getElement().getY() - scrollable.getPosition().y;
-        }
-
-        return alignmentInfo;
     }
 });
