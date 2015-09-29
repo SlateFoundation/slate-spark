5b32b86bd6ea4e9b02ade61ca9d1a317f3a70da2
Chris Alfano <chris@jarv.us>
2015-09-29 10:25:44 -0700

    Fix learn.Grid filename

---
+++ src/work/learn/Grid.js
@@ -0,0 +1,58 @@
+/*jslint browser: true, undef: true, laxcomma:true *//*global Ext*/
+Ext.define('SparkClassroom.work.learn.Grid', {
+    extend: 'Ext.grid.Grid',
+    xtype: 'spark-work-learn-grid',
+    requires: [
+        'Jarvus.plugin.GridFlex',
+        'Jarvus.plugin.GridHeight',
+        'SparkClassroom.column.Completed',
+        'SparkClassroom.column.Learn',
+        'SparkClassroom.column.DOK',
+        'SparkClassroom.column.LearnType',
+        'SparkClassroom.column.Rating',
+        'SparkClassroom.column.Score',
+        'SparkClassroom.column.Attachment'
+    ],
+
+    config: {
+        plugins: [
+            'gridflex',
+            'gridheight'
+        ],
+        grouped: true,
+        titleBar: null,
+        columns:[
+            {
+                dataIndex: 'completed',
+                xtype: 'spark-completed-column'
+            },
+            {
+                dataIndex: 'title',
+                xtype: 'spark-title-column',
+                flex: 2
+            },
+            {
+                dataIndex: 'dok',
+                xtype: 'spark-dok-column'
+            },
+            {
+                dataIndex: 'category',
+                xtype: 'spark-learntype-column'
+            },
+            {
+                dataIndex: 'rating',
+                xtype: 'spark-rating-column'
+            },
+            {
+                dataIndex: 'score',
+                xtype: 'spark-score-column'
+            },
+            {
+                dataIndex: 'attachments',
+                xtype: 'spark-attachment-column'
+            }
+        ],
+
+        store: 'work.Learns'
+    }
+});
