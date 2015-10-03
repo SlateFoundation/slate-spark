75286aa7fe557e111beb039831b751ac091fcefe
Bill Clinton <BillClinton@users.noreply.github.com>
2015-10-03 11:49:39 -0400

    Add references and listeners for stores

--- sencha-workspace/SparkClassroomStudent/app/controller/work/Conference.js
+++ sencha-workspace/SparkClassroomStudent/app/controller/work/Conference.js
@@ -2,6 +2,50 @@
 Ext.define('SparkClassroomStudent.controller.work.Conference', {
     extend: 'Ext.app.Controller',
 
+    stores: [
+        'work.ConferenceQuestions',
+        'work.ConferenceResources'
+    ],
+
+    refs: {
+        conferenceCt: 'spark-student-work-conference'
+    },
+
+    control: {
+        conferenceCt: {
+            activate: 'onConferenceCtActivate'
+        }
+    },
+
+    listen: {
+        store: {
+            '#work.ConferenceQuestions': {
+                load: 'onConferenceQuestionsStoreLoad'
+            }
+        }
+    },
+
+    onConferenceCtActivate: function(conferenceCt) {
+        console.log('onConferenceCtActivate');
+        var me = this,
+            store = me.getWorkConferenceQuestionsStore();
+
+        if(!store.isLoaded()) {
+            store.load();
+        }
+    },
 
     // TODO: handle loading data into conference section
+    onConferenceQuestionsStoreLoad: function(store, records) {
+        console.log('onConferenceQuestionsStoreLoad');
+        var resourcesStore = Ext.getStore('work.ConferenceResources'),
+            recordsLength = records.length,
+            i = 0;
+
+        for (i; i < recordsLength; i++) {
+            //console.log('hi');
+        }
+
+    }
+
 });
