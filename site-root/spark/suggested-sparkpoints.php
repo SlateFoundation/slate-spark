da4875d18de0024dc7f7fe6f7977fa88b40ea305
Chris Alfano <chris@jarv.us>
2015-10-05 16:58:50 +0000

    Use better sparkpoint codes for demo suggestions

--- site-root/spark/suggested-sparkpoints.php
+++ site-root/spark/suggested-sparkpoints.php
@@ -2,49 +2,49 @@
 
 JSON::respond([
     [
-        'code' => 'Math.G1.OA.1',
+        'code' => 'MATH.G9-12.APR.1',
         'group' => 'current'
     ],
     [
-        'code' => 'Math.G1.OA.2',
+        'code' => 'MATH.G9-12.APR.2',
         'group' => 'next'
     ],
     [
-        'code' => 'Math.G1.OA.3',
+        'code' => 'MATH.G9-12.APR.3',
         'group' => 'next',
         'recommended' => true
     ],
     [
-        'code' => 'Math.G1.OA.4',
+        'code' => 'MATH.G9-12.APR.4',
         'group' => 'next'
     ],
     [
-        'code' => 'Math.GK.OA.1',
+        'code' => 'MATH.GK.OA.1',
         'group' => 'past',
         'completed_date' => strtotime('2015-03-29')
     ],
     [
-        'code' => 'Math.GK.OA.2',
+        'code' => 'MATH.GK.OA.2',
         'group' => 'past',
         'completed_date' => strtotime('2015-03-20')
     ],
     [
-        'code' => 'Math.GK.OA.3',
+        'code' => 'MATH.GK.OA.3',
         'group' => 'past',
         'completed_date' => strtotime('2015-03-12')
     ],
     [
-        'code' => 'Math.GK.OA.4',
+        'code' => 'MATH.GK.OA.4',
         'group' => 'past',
         'completed_date' => strtotime('2015-03-01')
     ],
     [
-        'code' => 'Math.GK.OA.5',
+        'code' => 'MATH.GK.OA.5',
         'group' => 'past',
         'completed_date' => strtotime('2015-02-25')
     ],
     [
-        'code' => 'Math.GK.OA.6',
+        'code' => 'MATH.GK.OA.6',
         'group' => 'past',
         'completed_date' => strtotime('2015-02-13')
     ]
