9aca6cda89c3f2914634ff13f2c9f7385f50ce3b
Chris Alfano <chris@jarv.us>
2015-10-04 21:02:08 -0400

    Remove stores/models moved to classroom package

--- sencha-workspace/SparkClassroomStudent/app/store/work/ConferenceResources.js
+++
@@ -1,9 +0,0 @@
-/*jslint browser: true, undef: true *//*global Ext*/
-Ext.define('SparkClassroomStudent.store.work.ConferenceResources', {
-    extend: 'Ext.data.Store',
-    requires: [
-        'SparkClassroomStudent.model.work.ConferenceResource'
-    ],
-
-    model: 'SparkClassroomStudent.model.work.ConferenceResource'
-});
