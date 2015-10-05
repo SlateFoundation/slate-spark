8ba40a7995ed0ca8f4ce52e48d79d1dcb703ffb8
Chris Alfano <chris@jarv.us>
2015-10-04 21:10:10 -0400

    Remove extra container

--- src/work/conference/Main.js
+++ src/work/conference/Main.js
@@ -9,47 +9,43 @@ Ext.define('SparkClassroom.work.conference.Main', {
     ],
 
     config: {
-        itemId: 'conference',
-
         items: [
             {
-                xtype: 'container',
-                itemId: 'standardContainer',
+                layout: 'accordion',
                 items: [
                     {
+                        expanded: true,
+                        itemId: 'sparkpointCt',
+
                         xtype: 'container',
-                        layout: 'accordion',
+                        cls: 'content-card',
+                        title: '[Select a Sparkpoint]',
                         items: [
                             {
-                                xtype: 'container',
-                                cls: 'content-card',
-                                title: 'Standard 1 - CCSS.ELA.3.CC.4.A',
-                                items: [
-                                    {
-                                        xtype: 'spark-worklist',
-                                        itemId: 'questions',
-                                        data: {
-                                            title: 'Guiding Questions',
-                                            items: [
-                                                { text: 'Example <foo> of a first guiding question.' },
-                                                { text: 'Example of a second guiding question that a student should be prepared to respond to.' },
-                                                { text: 'Example of a third guiding question that a student should be prepared to respond to.', studentSubmitted: true },
-                                                { text: '<div class="inline-flex-fullwidth-ct"><input placeholder="Add a guiding question you want to discuss with the teacher (optional)" class="flex-1"> <button type="submit">Add</button></div>', skipHtmlEncode: true }
-                                            ]
-                                        }
-                                    },
-                                    {
-                                        xtype: 'spark-worklist',
-                                        itemId: 'resources',
-                                        data: {
-                                            title: 'Resources',
-                                            items: [
-                                                { text: 'Title of resource', linkTitle: 'documenttoshare.pdf', linkUrl: '#' },
-                                                { text: 'Title of resource', linkUrl: 'http://example.com' }
-                                            ]
-                                        }
-                                    }
-                                ]
+                                itemId: 'questions',
+
+                                xtype: 'spark-worklist',
+                                data: {
+                                    title: 'Guiding Questions',
+                                    items: [
+                                        { text: 'Example <foo> of a first guiding question.' },
+                                        { text: 'Example of a second guiding question that a student should be prepared to respond to.' },
+                                        { text: 'Example of a third guiding question that a student should be prepared to respond to.', studentSubmitted: true },
+                                        { text: '<div class="inline-flex-fullwidth-ct"><input placeholder="Add a guiding question you want to discuss with the teacher (optional)" class="flex-1"> <button type="submit">Add</button></div>', skipHtmlEncode: true }
+                                    ]
+                                }
+                            },
+                            {
+                                itemId: 'resources',
+
+                                xtype: 'spark-worklist',
+                                data: {
+                                    title: 'Resources',
+                                    items: [
+                                        { text: 'Title of resource', linkTitle: 'documenttoshare.pdf', linkUrl: '#' },
+                                        { text: 'Title of resource', linkUrl: 'http://example.com' }
+                                    ]
+                                }
                             }
                         ]
                     }
