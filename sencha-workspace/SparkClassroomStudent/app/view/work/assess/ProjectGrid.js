40cf2e386e210aec111400c210c5c4c666d3a81d
Chris Alfano <chris@jarv.us>
2015-10-06 12:54:56 -0400

    Expand learn/apply student ratings scale to 1-10

--- sencha-workspace/SparkClassroomStudent/app/view/work/assess/ProjectGrid.js
+++ sencha-workspace/SparkClassroomStudent/app/view/work/assess/ProjectGrid.js
@@ -30,6 +30,11 @@ Ext.define('SparkClassroomStudent.view.work.assess.AppliesGrid', {
                 },
                 tpl: [
                     '<select>',
+                        '<option>10</option>',
+                        '<option>9</option>',
+                        '<option>8</option>',
+                        '<option>7</option>',
+                        '<option>6</option>',
                         '<option>5</option>',
                         '<option>4</option>',
                         '<option>3</option>',
