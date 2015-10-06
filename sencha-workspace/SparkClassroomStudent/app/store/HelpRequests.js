706ca40fa00531cca8dc96040a8b669570a7cfc8
Chris Alfano <chris@jarv.us>
2015-10-06 19:45:01 -0400

    Populate help requests with mock data

--- sencha-workspace/SparkClassroomStudent/app/store/HelpRequests.js
+++ sencha-workspace/SparkClassroomStudent/app/store/HelpRequests.js
@@ -1,9 +1,60 @@
 /*jslint browser: true, undef: true *//*global Ext*/
 Ext.define('SparkClassroomStudent.store.HelpRequests', {
     extend: 'Ext.data.Store',
-    requires: [
-        'SparkClassroomStudent.model.HelpRequest'
-    ],
 
-    model: 'SparkClassroomStudent.model.HelpRequest'
+
+    model: 'SparkClassroomStudent.model.HelpRequest',
+
+    config: {
+        data: [
+            {
+                Student: {
+                    FirstName: 'Sherrill',
+                    LastName: 'Scherf'
+                },
+                Created: new Date(Date.now() - 5 * 1000),
+                Type: 'question-general'
+            },
+            {
+                Student: {
+                    FirstName: 'Clarisa',
+                    LastName: 'Cross'
+                },
+                Created: new Date(Date.now() - 50 * 1000),
+                Type: 'bathroom'
+            },
+            {
+                Student: {
+                    FirstName: 'Shon',
+                    LastName: 'Simoneaux'
+                },
+                Created: new Date(Date.now() - 150 * 1000),
+                Type: 'locker'
+            },
+            {
+                Student: {
+                    FirstName: 'Edmund',
+                    LastName: 'Ebel'
+                },
+                Created: new Date(Date.now() - 65 * 1000),
+                Type: 'nurse'
+            },
+            {
+                Student: {
+                    FirstName: 'Jenise',
+                    LastName: 'Jiang'
+                },
+                Created: new Date(Date.now() - 10 * 1000),
+                Type: 'question-academic'
+            },
+            {
+                Student: {
+                    FirstName: 'Tiffany',
+                    LastName: 'To'
+                },
+                Created: new Date(Date.now() - 300 * 1000),
+                Type: 'question-technology'
+            }
+        ]
+    }
 });
