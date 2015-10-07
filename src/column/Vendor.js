e49b282dc96af1909a7eac8c2a672bb9dade8c26
Chris Alfano <chris@jarv.us>
2015-10-07 09:28:35 -0400

    Update vendor column

--- src/column/Vendor.js
+++ src/column/Vendor.js
@@ -13,13 +13,13 @@ Ext.define('SparkClassroom.column.Vendor', {
         tpl: [
             // TODO fix/replace default vendor image and url
             '<div class="spark-grid-row-image small" style="background-image:url(',
-                '<tpl if="VendorImage">',
-                    '{VendorImage}',
+                '<tpl if="vendor_logo">',
+                    '{vendor_logo}',
                 '<tpl else>',
                     '/spark-classroom-student/sencha-workspace/build/production/SparkClassroomStudent/resources/images/64x64.png',
                 '</tpl>',
             ')"></div>',
-            '{Vendor}'
+            '{vendor}'
         ]
     }
 });
