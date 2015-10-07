d37b85efc69bec26614f6e1994b817812001ca47
Chris Alfano <chris@jarv.us>
2015-10-07 09:28:26 -0400

    Revert title column to old simpler form

--- src/column/Title.js
+++ src/column/Title.js
@@ -3,26 +3,9 @@ Ext.define('SparkClassroom.column.Title', {
     xtype: 'spark-title-column',
 
     config: {
+        dataIndex: 'title',
         cls: 'spark-title-column',
         flex: 2,
-        dataIndex: 'title',
-        cell: {
-            encodeHtml: false
-        },
-        tpl: [
-            '<tpl if="thumbnail">',
-                '<img src="{thumbnail:htmlEncode}" height="26"> ',
-            '</tpl>',
-
-            '<tpl if="url">',
-                '<a href="{url:htmlEncode}" target="_blank">',
-            '</tpl>',
-
-            '{title}',
-
-            '<tpl if="url">',
-                '</a>',
-            '</tpl>'
-        ]
+        text: 'Title'
     }
 });
