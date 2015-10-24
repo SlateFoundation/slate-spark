5032503e29fa5b359f1dc0c3d880a7360ff0fa83
Chris Alfano <chris@jarv.us>
2015-10-24 17:07:04 -0400

    Rename assess.Main -> assess.Container

--- sencha-workspace/SparkClassroomStudent/app/view/work/assess/Container.js
+++ sencha-workspace/SparkClassroomStudent/app/view/work/assess/Container.js
@@ -1,6 +1,6 @@
 /*jslint browser: true, undef: true, laxcomma:true *//*global Ext*/
 Ext.define('SparkClassroomStudent.view.work.assess.Container', {
-    extend: 'SparkClassroom.work.assess.Main',
+    extend: 'SparkClassroom.work.assess.Container',
     xtype: 'spark-student-work-assess',
     requires: [
         'SparkClassroomStudent.view.work.assess.AppliesGrid',
