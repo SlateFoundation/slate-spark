6dd98907305dcf395b89bd745f3a7e74a091d019
Chris Alfano <chris@jarv.us>
2015-10-24 15:15:41 -0400

    Use new learn.Container name

--- sencha-workspace/SparkClassroomStudent/app/view/work/learn/Container.js
+++ sencha-workspace/SparkClassroomStudent/app/view/work/learn/Container.js
@@ -1,6 +1,6 @@
 /*jslint browser: true, undef: true, laxcomma:true *//*global Ext*/
 Ext.define('SparkClassroomStudent.view.work.learn.Container', {
-    extend: 'SparkClassroom.work.learn.Main',
+    extend: 'SparkClassroom.work.learn.Container',
     xtype: 'spark-student-work-learn',
 
     initialize: function () {
