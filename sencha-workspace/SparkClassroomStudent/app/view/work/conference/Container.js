6a9622d098b2312e34284bcc13edb54bcadf4cca
Chris Alfano <chris@jarv.us>
2015-10-24 15:22:02 -0400

    Rename conference.Main -> conference.Container

--- sencha-workspace/SparkClassroomStudent/app/view/work/conference/Container.js
+++ sencha-workspace/SparkClassroomStudent/app/view/work/conference/Container.js
@@ -1,6 +1,6 @@
 /*jslint browser: true, undef: true, laxcomma:true *//*global Ext*/
 Ext.define('SparkClassroomStudent.view.work.conference.Container', {
-    extend: 'SparkClassroom.work.conference.Main',
+    extend: 'SparkClassroom.work.conference.Container',
     xtype: 'spark-student-work-conference',
     requires: [
         'SparkClassroomStudent.view.work.conference.PeerForm'
