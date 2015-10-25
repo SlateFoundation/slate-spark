a0491976c584f4a810562bb1f34471827dd8f995
Chris Alfano <chris@jarv.us>
2015-10-25 11:01:26 -0400

    Update learns grid in real time

--- sencha-workspace/SparkClassroomTeacher/app/controller/work/Learn.js
+++ sencha-workspace/SparkClassroomTeacher/app/controller/work/Learn.js
@@ -30,6 +30,9 @@ Ext.define('SparkClassroomTeacher.controller.work.Learn', {
             '#': {
                 activestudentselect: 'onActiveStudentSelect'
             }
+        },
+        socket: {
+            data: 'onSocketData'
         }
     },
 
@@ -63,6 +66,38 @@ Ext.define('SparkClassroomTeacher.controller.work.Learn', {
         this.syncActiveSparkpoint();
     },
 
+    onSocketData: function(socket, data) {
+        if (data.table != 'learn_activity') {
+            return;
+        }
+
+        var me = this,
+            activeStudent = me.getActiveStudent(),
+            itemData = data.item,
+            updatedLearn;
+
+        if (!activeStudent || activeStudent.getId() != itemData.user_id) {
+            return;
+        }
+
+        updatedLearn = me.getLearnGrid().getStore().getById(itemData.resource_id);
+
+        if (updatedLearn) {
+            // TODO: can we find ways to not duplicate this logic between the api and the client?
+            // Can there be an abstraction on the server side so that a higher-level event comes down
+            // with a delta to the object as returned by the API previously so we can just pass the whole
+            // data object to set?
+            updatedLearn.set({
+                launched: itemData.start_status == 'launched',
+                completed: itemData.completed
+            },{
+                dirty: false
+            });
+        }
+    },
+
+
+    // controller methods
     syncActiveSparkpoint: function() {
         var me = this,
             learnCt = me.getLearnCt(),
