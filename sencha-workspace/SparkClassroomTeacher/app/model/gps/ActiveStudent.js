9981caacd6474c4a1c748876409cb73355581f35
Chris Alfano <chris@jarv.us>
2015-10-25 22:02:39 -0400

    Extend StudentSparkpoint with ActiveStudent

--- sencha-workspace/SparkClassroomTeacher/app/model/gps/ActiveStudent.js
+++ sencha-workspace/SparkClassroomTeacher/app/model/gps/ActiveStudent.js
@@ -1,61 +1,13 @@
 /*jslint browser: true, undef: true *//*global Ext*/
 Ext.define('SparkClassroomTeacher.model.gps.ActiveStudent', {
-    extend: 'Ext.data.Model',
+    extend: 'SparkClassroom.model.StudentSparkpoint',
 
-    idProperty: 'user_id',
+    idProperty: 'student_id',
     fields: [
         {
-            name: 'user_id',
-            type: 'integer'
-        },
-        {
-            name: 'sparkpoint_id',
-            type: 'string'
-        },
-        {
-            name: 'sparkpoint_code',
-            type: 'string'
-        },
-        {
-            name: 'section_id',
-            type: 'string'
-        },
-        {
-            name: 'learn_duration',
-            type: 'integer'
-        },
-        {
-            name: 'conference_duration',
-            type: 'integer'
-        },
-        {
-            name: 'apply_duration',
-            type: 'integer'
-        },
-        {
-            name: 'assess_duration',
-            type: 'integer'
-        },
-        {
-            name: 'phase',
-            type: 'string'
-        },
-        {
-            name: 'complete',
-            type: 'boolean'
-        },
-        {
-            name: 'last_active',
-            type: 'date'
-        },
-        {
-            name: 'metatdata'
-        },
-
-        {
             name: 'student',
-            depends: ['user_id'],
-            mapping: 'user_id',
+            depends: ['student_id'],
+            mapping: 'student_id',
             convert: function(v, r) {
                 return Ext.getStore('Students').getById(v);
             }
