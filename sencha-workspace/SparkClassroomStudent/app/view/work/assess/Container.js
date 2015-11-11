8079f75e03ad3d0e2f6dfaa250b588d1c08dec0d
Chris Alfano <chris@jarv.us>
2015-11-10 21:56:50 -0500

    Move applies grid for assess phase to shared package

--- sencha-workspace/SparkClassroomStudent/app/view/work/assess/Container.js
+++ sencha-workspace/SparkClassroomStudent/app/view/work/assess/Container.js
@@ -3,7 +3,6 @@ Ext.define('SparkClassroomStudent.view.work.assess.Container', {
     extend: 'SparkClassroom.work.assess.Container',
     xtype: 'spark-student-work-assess',
     requires: [
-        'SparkClassroomStudent.view.work.assess.AppliesGrid',
         'SparkClassroomStudent.view.work.assess.Footer'
     ],
 
@@ -11,9 +10,6 @@ Ext.define('SparkClassroomStudent.view.work.assess.Container', {
         this.callParent(arguments);
 
         this.add([
-            {
-                xtype: 'spark-student-work-assess-appliesgrid'
-            },
             {
                 xtype: 'spark-student-work-assess-footer',
                 margin: '0 0 100'
