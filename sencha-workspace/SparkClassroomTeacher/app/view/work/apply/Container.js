fbe91b43454e4aea46f2f7a875a9a41f9b7258d9
Chris Alfano <chris@jarv.us>
2015-11-10 22:16:44 -0500

    Remove double bagging in apply

--- sencha-workspace/SparkClassroomTeacher/app/view/work/apply/Container.js
+++ sencha-workspace/SparkClassroomTeacher/app/view/work/apply/Container.js
@@ -141,61 +141,52 @@ Ext.define('SparkClassroomTeacher.view.work.apply.Container', {
                         layout: 'hbox',
                         margin: '0 -12',
                         defaults: {
-                            padding: 12,
-                            defaults: {
-                                xtype: 'spark-panel'
-                            }
+                            xtype: 'spark-panel',
+                            padding: 12
                         },
                         items: [
                             {
+                                itemId: 'reflectionCmp',
                                 flex: 2,
-                                items: [
-                                    {
-                                        itemId: 'reflectionCmp',
 
-                                        title: 'Reflection',
-                                        tpl: '{[fm.nl2br(fm.htmlEncode(values.reflection))]}'
-                                    }
-                                ]
+                                    title: 'Reflection',
+                                    tpl: '{[fm.nl2br(fm.htmlEncode(values.reflection))]}'
                             },
                             {
                                 flex: 1,
+
+                                title: 'Submitted Docs',
                                 items: [
                                     {
-                                        title: 'Submitted Docs',
-                                        items: [
-                                            {
-                                                itemId: 'submissionsView',
+                                        itemId: 'submissionsView',
 
-                                                xtype: 'dataview',
-                                                cls: 'link-list', // has-checkboxes // TODO: move back into classes
-                                                itemCls: 'link-list-item',
-                                                emptyText: 'None attached yet',
-                                                deferEmptyText: false,
-                                                allowDeselect: true,
-                                                store: {
-                                                    // TODO: move to model
-                                                    idProperty: 'url',
-                                                    fields: [
-                                                        {
-                                                            name: 'id',
-                                                            mapping: 'url',
-                                                        },
-                                                        {
-                                                            name: 'url'
-                                                        },
-                                                        {
-                                                            name: 'title',
-                                                            depends: ['url'],
-                                                            convert: function(v, r) {
-                                                                return v || r.get('url').replace(/^https?:\/\//, '');
-                                                            }
-                                                        },
-                                                    ]
+                                        xtype: 'dataview',
+                                        cls: 'link-list', // has-checkboxes // TODO: move back into classes
+                                        itemCls: 'link-list-item',
+                                        emptyText: 'None attached yet',
+                                        deferEmptyText: false,
+                                        allowDeselect: true,
+                                        store: {
+                                            // TODO: move to model
+                                            idProperty: 'url',
+                                            fields: [
+                                                {
+                                                    name: 'id',
+                                                    mapping: 'url',
                                                 },
-                                                itemTpl: '<a href="{url:htmlEncode}" title="{title:htmlEncode}" target=_blank>{title:htmlEncode}</a>'
-                                            }
-                                        ]
+                                                {
+                                                    name: 'url'
+                                                },
+                                                {
+                                                    name: 'title',
+                                                    depends: ['url'],
+                                                    convert: function(v, r) {
+                                                        return v || r.get('url').replace(/^https?:\/\//, '');
+                                                    }
+                                                },
+                                            ]
+                                        },
+                                        itemTpl: '<a href="{url:htmlEncode}" title="{title:htmlEncode}" target=_blank>{title:htmlEncode}</a>'
                                     }
                                 ]
                             }
