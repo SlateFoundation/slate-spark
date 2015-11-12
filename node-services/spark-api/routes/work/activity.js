2c86bc8c3b31a77ca91e6e50f7bebd17d36d4653
Jeffrey Mealo <jeffreymealo@gmail.com>
2015-11-11 23:58:39 -0500

    Remove hardcoded table from query in GET /work/activity

--- node-services/spark-api/routes/work/activity.js
+++ node-services/spark-api/routes/work/activity.js
@@ -10,7 +10,20 @@ function *getHandler() {
                    t.section_id,
                    t.student_id,
                    t.sparkpoint_id,
-                   learn_start_time, learn_finish_time, conference_start_time, conference_finish_time, conference_join_time, apply_start_time, apply_ready_time, apply_finish_time, assess_start_time, assess_ready_time, assess_finish_time, conference_group_id, selected_apply_id, selected_fb_apply_id,
+                   learn_start_time,
+                   learn_finish_time,
+                   conference_start_time,
+                   conference_finish_time,
+                   conference_join_time,
+                   apply_start_time,
+                   apply_ready_time,
+                   apply_finish_time,
+                   assess_start_time,
+                   assess_ready_time,
+                   assess_finish_time,
+                   conference_group_id,
+                   selected_apply_id,
+                   selected_fb_apply_id,
                    code AS sparkpoint
               FROM (
                 SELECT student_id,
@@ -20,7 +33,7 @@ function *getHandler() {
                        ROW_NUMBER() OVER (
                          PARTITION BY ssas.student_id, ssas.section_id
                              ORDER BY ssas.last_accessed desc) AS rn
-                  FROM "sandbox-school".section_student_active_sparkpoint ssas
+                  FROM section_student_active_sparkpoint ssas
                  WHERE section_id = $1 AND last_accessed IS NOT NULL
               ) t
             LEFT JOIN student_sparkpoint ss ON ss.sparkpoint_id = t.sparkpoint_id
