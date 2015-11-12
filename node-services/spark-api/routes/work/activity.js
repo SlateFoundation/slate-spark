2930d9385cc800f2d4ecec5de2a8d95e597507c8
Jeffrey Mealo <jeffreymealo@gmail.com>
2015-11-11 23:44:23 -0500

    Fix GET /work/activity query to show freshly selected sparkpoints

--- node-services/spark-api/routes/work/activity.js
+++ node-services/spark-api/routes/work/activity.js
@@ -8,22 +8,25 @@ function *getHandler() {
         query = `
             SELECT t.last_accessed,
                    t.section_id,
-                   ss.*,
+                   t.student_id,
+                   t.sparkpoint_id,
+                   learn_start_time, learn_finish_time, conference_start_time, conference_finish_time, conference_join_time, apply_start_time, apply_ready_time, apply_finish_time, assess_start_time, assess_ready_time, assess_finish_time, conference_group_id, selected_apply_id, selected_fb_apply_id,
                    code AS sparkpoint
               FROM (
-                       SELECT student_id,
-                              sparkpoint_id,
-                              section_id,
-                              last_accessed,
-                              ROW_NUMBER() OVER (
-                                PARTITION BY ssas.student_id, ssas.section_id
-                                    ORDER BY ssas.last_accessed desc) AS rn
-                         FROM "sandbox-school".section_student_active_sparkpoint ssas
-                         WHERE section_id = $1 AND last_accessed IS NOT NULL
-                    ) t
-              JOIN student_sparkpoint ss ON ss.sparkpoint_id = t.sparkpoint_id AND ss.student_id = t.student_id
-              JOIN sparkpoints ON sparkpoints.id = t.sparkpoint_id
-              WHERE t.rn = 1;`;
+                SELECT student_id,
+                       sparkpoint_id,
+                       section_id,
+                       last_accessed,
+                       ROW_NUMBER() OVER (
+                         PARTITION BY ssas.student_id, ssas.section_id
+                             ORDER BY ssas.last_accessed desc) AS rn
+                  FROM "sandbox-school".section_student_active_sparkpoint ssas
+                 WHERE section_id = $1 AND last_accessed IS NOT NULL
+              ) t
+            LEFT JOIN student_sparkpoint ss ON ss.sparkpoint_id = t.sparkpoint_id
+                  AND ss.student_id = t.student_id
+                 JOIN sparkpoints ON sparkpoints.id = t.sparkpoint_id
+                WHERE t.rn = 1;`;
 
     records = yield this.pgp.manyOrNone(query, [sectionId]);
 
