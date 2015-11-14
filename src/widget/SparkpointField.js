bb5a92fcafb08d7cc28b0ca26ceed9efdf96fed3
Chris Alfano <chris@jarv.us>
2015-11-13 22:50:48 -0500

    Move suggestions list management inside sparkpoint field class

--- src/widget/SparkpointField.js
+++ src/widget/SparkpointField.js
@@ -1,12 +1,88 @@
+/* global SparkClassroom */
 Ext.define('SparkClassroom.widget.SparkpointField', {
     extend: 'Ext.field.Text',
     xtype: 'spark-sparkpointfield',
+    requires: [
+        'SparkClassroom.widget.SparkpointSuggestions'
+    ],
 
 
     config: {
+        selectedSparkpoint: null,
+        query: null,
+        suggestionsList: true,
+
         cls: 'spark-navbar-sparkpoint-selector',
         label: 'Sparkpoint',
         labelCls: 'visually-hidden',
-        placeHolder: 'Select Sparkpoint'
+        placeHolder: 'Select Sparkpoint',
+
+        listeners: {
+            focus: 'onFieldFocus',
+            change: { fn: 'onFieldChange', buffer: 250 }
+        }
+    },
+
+
+    applySuggestionsList: function(config, oldList) {
+        if (config === true) {
+            config = {};
+        }
+
+        return Ext.factory(config, SparkClassroom.widget.SparkpointSuggestions, oldList);
+    },
+
+    updateSuggestionsList: function(suggestionsList) {
+        suggestionsList.on({
+            scope: this,
+            select: 'onSuggestionsListSelect'
+        });
+    },
+
+    updateQuery: function(query) {
+        var loadConfig = {};
+
+        if (query) {
+            loadConfig.url = '/spark/api/sparkpoints/autocomplete';
+            loadConfig.params = {
+                q: query
+            };
+        }
+
+        this.getSuggestionsList().getStore().load(loadConfig);
+    },
+
+    updateSelectedSparkpoint: function(sparkpoint, oldSparkpoint) {
+        var me = this;
+
+        me.setValue(sparkpoint.getId());
+        me.fireEvent('sparkpointselect', me, sparkpoint, oldSparkpoint);
+    },
+
+    onFieldFocus: function(sparkpointField) {
+        var me = this,
+            suggestionsList = me.getSuggestionsList();
+
+        suggestionsList.setVisibility(false);
+
+        if (!suggestionsList.getParent()) {
+            (me.up('{getScrollable()}') || Ext.Viewport).add(suggestionsList);
+        }
+
+        suggestionsList.show();
+        suggestionsList.alignTo(sparkpointField, 'tl-bl');
+
+        me.setQuery(sparkpointField.getValue()||'');
+
+        suggestionsList.setVisibility(true);
+    },
+
+    onFieldChange: function(sparkpointField, value) {
+        this.setQuery(value);
+    },
+
+    onSuggestionsListSelect: function(suggestionsList, sparkpoint) {
+        this.setSelectedSparkpoint(sparkpoint);
+        suggestionsList.hide();
     }
 });
