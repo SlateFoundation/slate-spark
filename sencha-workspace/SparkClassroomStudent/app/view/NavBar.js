f8fc535d466f25288e8e9a68260f83da5ae75440
Chris Alfano <chris@jarv.us>
2015-11-13 22:50:48 -0500

    Move suggestions list management inside sparkpoint field class

--- sencha-workspace/SparkClassroomStudent/app/view/NavBar.js
+++ sencha-workspace/SparkClassroomStudent/app/view/NavBar.js
@@ -5,22 +5,10 @@ Ext.define('SparkClassroomStudent.view.NavBar', {
     requires: [
         'Ext.field.Text',
         'Ext.dataview.List',
-        'SparkClassroom.widget.SparkpointField',
-        'SparkClassroom.widget.SparkpointSuggestions'
+        'SparkClassroom.widget.SparkpointField'
     ],
 
     config: {
-        selectedSparkpoint: null,
-        sparkpointQuery: null,
-        sparkpointsList: true,
-
-        control: {
-            'spark-sparkpointfield': {
-                focus: 'onSparkpointFieldFocus',
-                change: { fn: 'onSparkpointFieldChange', buffer: 250 }
-            }
-        },
-
         layout: {
             type: 'hbox',
             pack: 'center'
@@ -64,68 +52,5 @@ Ext.define('SparkClassroomStudent.view.NavBar', {
                 disabled: true
             }
         ]
-    },
-
-    applySparkpointsList: function(config, oldList) {
-        if (config === true) {
-            config = {};
-        }
-
-        return Ext.factory(config, SparkClassroom.widget.SparkpointSuggestions, oldList);
-    },
-
-    updateSparkpointsList: function(sparkpointsList) {
-        sparkpointsList.on({
-            scope: this,
-            select: 'onSparkpointsListSelect'
-        });
-    },
-
-    updateSparkpointQuery: function(query) {
-        var loadConfig = {};
-
-        if (query) {
-            loadConfig.url = '/spark/api/sparkpoints/autocomplete';
-            loadConfig.params = {
-                q: query
-            };
-        }
-
-        this.getSparkpointsList().getStore().load(loadConfig);
-    },
-
-    updateSelectedSparkpoint: function(sparkpoint, oldSparkpoint) {
-        var me = this,
-            sparkpointSelector = me.down('spark-sparkpointfield');
-
-        sparkpointSelector.setValue(sparkpoint.getId());
-        sparkpointSelector.fireEvent('sparkpointselect', sparkpointSelector, sparkpoint, oldSparkpoint);
-    },
-
-    onSparkpointFieldFocus: function(sparkpointField) {
-        var me = this,
-            sparkpointsList = me.getSparkpointsList();
-
-        sparkpointsList.setVisibility(false);
-
-        if (!sparkpointsList.getParent()) {
-            (me.up('{getScrollable()}') || Ext.Viewport).add(sparkpointsList);
-        }
-
-        sparkpointsList.show();
-        sparkpointsList.alignTo(sparkpointField, 'tl-bl');
-
-        me.setSparkpointQuery(sparkpointField.getValue()||'');
-
-        sparkpointsList.setVisibility(true);
-    },
-
-    onSparkpointFieldChange: function(sparkpointField, value) {
-        this.setSparkpointQuery(value);
-    },
-
-    onSparkpointsListSelect: function(sparkpointsList, sparkpoint) {
-        this.setSelectedSparkpoint(sparkpoint);
-        sparkpointsList.hide();
     }
 });
