840ce1e4b505c9fd7345a3f6c4c1ededc5ac91b1
Chris Alfano <chris@jarv.us>
2015-11-13 20:49:38 -0500

    Add spark-sparkpointslookup store

---
+++ src/store/SparkpointsLookup.js
@@ -0,0 +1,58 @@
+/*jslint browser: true, undef: true *//*global Ext*/
+Ext.define('SparkClassroom.store.SparkpointsLookup', {
+    extend: 'Ext.data.Store',
+    alias: 'store.spark-sparkpointslookup',
+    requires: [
+        'Slate.proxy.API',
+        'SparkClassroom.model.Sparkpoint'
+    ],
+
+
+    config: {
+        model: 'SparkClassroom.model.Sparkpoint',
+        proxy: {
+            type: 'slate-api',
+            url: '/spark/api/sparkpoints/suggested'
+        },
+        grouper: {
+            groupFn: function(r) {
+                if (r.get('assess_finish_time')) {
+                    return 'Completed Sparkpoints';
+                }
+
+                if (r.get('learn_start_time')) {
+                    return 'Current';
+                }
+
+                return 'Next Up';
+            },
+            sorterFn: function(r1, r2) {
+                var assessFinish1 = r1.get('assess_finish_time'),
+                    assessFinish2 = r2.get('assess_finish_time'),
+                    current1 = r1.get('learn_start_time') && !assessFinish1,
+                    current2 = r2.get('learn_start_time') && !assessFinish2;
+debugger;
+                // current first
+                if (current1 && !current2) {
+                    return -1;
+                } else if (!current1 && current2) {
+                    return 1;
+                }
+
+                // next up second
+                if (assessFinish1 && !assessFinish2) {
+                    return -1;
+                } else if (!assessFinish1 && assessFinish2) {
+                    return 1;
+                }
+
+                // completed
+                return 0;
+            }
+        },
+        sorters: [{
+            property: 'last_accessed',
+            direction: 'DESC'
+        }]
+    }
+});
