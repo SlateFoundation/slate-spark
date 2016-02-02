abe7ac811e480db6301b67d14bd84f59afdc9001
Jeffrey Mealo <jeffreymealo@gmail.com>
2016-02-02 15:05:33 -0500

    Add request body to logs temporarily

--- node-services/spark-api/middleware/logger.js
+++ node-services/spark-api/middleware/logger.js
@@ -9,7 +9,9 @@ module.exports = function *logger(next) {
 
     if (process.env.NODE_ENV === 'production') {
         console.log('[%s] - %s %s - %s - %s', start.toUTCString(), this.method, this.url, ms, 'ms', this.requestId);
+        console.log(this.request.body);
     } else {
         console.log('[%s] - %s %s - %s - %s', start.toUTCString(), this.method, this.url, ms, 'ms');
+        console.log(this.request.body);
     }
 };
