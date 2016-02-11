1f31c744aa41ffa78e1571dab996f4c2b914897a
Chris Alfano <chris@jarv.us>
2016-02-10 22:10:13 -0500

    Remove unused SectionStudent store/model from early prototype

--- src/store/SectionStudents.js
+++
@@ -1,7 +0,0 @@
-/*jslint browser: true, undef: true *//*global Ext*/
-Ext.define('SparkClassroom.store.SectionStudents', {
-    extend: 'Ext.data.Store',
-
-
-    model: 'SparkClassroom.model.SectionStudent'
-});
