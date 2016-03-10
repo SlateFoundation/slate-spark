7e08f090fccecda0dfb5eb22e627b418b4b14b2f
Chris Alfano <chris@jarv.us>
2016-03-10 01:08:40 -0500

    Add TODO notes on realtime refresh for priorities list

--- sencha-workspace/SparkClassroomTeacher/app/controller/Priorities.js
+++ sencha-workspace/SparkClassroomTeacher/app/controller/Priorities.js
@@ -27,6 +27,11 @@ Ext.define('SparkClassroomTeacher.controller.Priorities', {
     },
 
     onSocketData: function(socket, data) {
-
+        // TODO: make like the GPS controller and blindly refresh the whole priorities store each time an
+        //       event is seen that may indicate new data. We need data from two tables to know what should
+        //       be in the list and dealing with that is pretty hairy right now.
+        //       - a student_section_active_sparkpoint record being inserted to this section should trigger a refresh
+        //       - a student_sparkpoint record updating that matches a student loaded into our Students store should
+        //         trigger a refresh
     }
 });
