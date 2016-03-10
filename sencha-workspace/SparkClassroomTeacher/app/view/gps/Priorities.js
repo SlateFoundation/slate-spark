1a30287ec5f6ff76201f9a5411eb63caa6ddb049
Chris Alfano <chris@jarv.us>
2016-03-09 23:39:44 -0500

    New teacher-only components should use the `spark-teacher-` xtype prefix

--- sencha-workspace/SparkClassroomTeacher/app/view/gps/Priorities.js
+++ sencha-workspace/SparkClassroomTeacher/app/view/gps/Priorities.js
@@ -1,7 +1,7 @@
 /*jslint browser: true, undef: true, laxcomma:true *//*global Ext*/
 Ext.define('SparkClassroomTeacher.view.gps.Priorities', {
     extend: 'SparkClassroomTeacher.view.gps.StudentList',
-    xtype: 'spark-priorities',
+    xtype: 'spark-teacher-priorities',
 
 
     config: {
