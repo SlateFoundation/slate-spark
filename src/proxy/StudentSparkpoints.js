13f249dc3e60567a3d6b50435af009eeae647e6f
Chris Hoopes <choopes@gmail.com>
2016-03-11 16:19:11 -0500

    Add new StudentSparkpoints proxy

---
+++ src/proxy/StudentSparkpoints.js
@@ -0,0 +1,26 @@
+/*jslint browser: true, undef: true *//*global Ext*/
+Ext.define('SparkClassroom.proxy.StudentSparkpoints', {
+    extend: 'Slate.proxy.API',
+    alias: 'proxy.spark-studentsparkpoints',
+
+    config: {
+        reader: {
+            type: 'json',
+            transform: function(data) {
+                var i = 0, length = data.length,
+                    record;
+
+                for (; i < length; i++) {
+                    record = data[i];
+                    record.student_sparkpoint = record.student_id + '-' + record.sparkpoint_id;
+                }
+                return data;
+            }
+        },
+
+        writer: {
+            type: 'json',
+            allowSingle: true
+        }
+    }
+});
