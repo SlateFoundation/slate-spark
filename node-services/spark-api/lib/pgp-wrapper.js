91bf62d4a6e9728cdad0641fcea14e06db415fdb
Jeffrey Mealo <jeffreymealo@gmail.com>
2016-03-22 08:50:24 -0400

    PgpWrapper should provide a task method and only use .call and .apply where necessary

--- node-services/spark-api/lib/pgp-wrapper.js
+++ node-services/spark-api/lib/pgp-wrapper.js
@@ -1,36 +1,54 @@
 'use strict';
 
+require('generator-bind').polyfill();
+
 function PgpWrapper(pgp, ctx) {
     this.pgp = pgp;
     this.ctx = ctx;
 }
 
+/* This wrapper is verbose and minimalistic in order to optimise for:
+    1. Garbage collection [each instance is just two references with methods on prototype, not each instance]
+    2. Execution speed / V8 Optimization [ES6/ES5 meta-programming features are slow. V8 can optimise simple objects.
+    3. Code auto-completion [explicitly defining the methods should work with most IDEs, looping over an array may not]
+*/
+
 PgpWrapper.prototype.none = function(query, values) {
-    return this.pgp.none.call(this.pgp, this.ctx.guc() + query, values);
+    return this.pgp.none(this.ctx.guc() + query, values);
 };
 
 PgpWrapper.prototype.one = function(query, values) {
-    return this.pgp.one.call(this.pgp, this.ctx.guc() + query, values);
+    return this.pgp.one(this.ctx.guc() + query, values);
 };
 
 PgpWrapper.prototype.many = function(query, values) {
-    return this.pgp.many.call(this.pgp, this.ctx.guc() + query, values);
+    return this.pgp.many(this.ctx.guc() + query, values);
 };
 
 PgpWrapper.prototype.oneOrNone = function(query, values) {
-    return this.pgp.oneOrNone.call(this.pgp, this.ctx.guc() + query, values);
+    return this.pgp.oneOrNone(this.ctx.guc() + query, values);
 };
 
 PgpWrapper.prototype.manyOrNone = function(query, values) {
-    return this.pgp.manyOrNone.call(this.pgp, this.ctx.guc() + query, values);
+    return this.pgp.manyOrNone(this.ctx.guc() + query, values);
 };
 
 PgpWrapper.prototype.any = function(query, values) {
-    return this.pgp.any.call(this.pgp, this.ctx.guc() + query, values);
+    return this.pgp.any(this.ctx.guc() + query, values);
 };
 
 PgpWrapper.prototype.result = function(query, values) {
-    return this.pgp.result.call(this.pgp, this.ctx.guc() + query, values);
+    return this.pgp.result(this.ctx.guc() + query, values);
+};
+
+PgpWrapper.prototype.task = function (p1, p2) {
+    p1 = p1.bind({}, this);
+
+    if (p2) {
+        p2 = p2.bind({}, this);
+    }
+
+    return this.pgp.task(p1, p2);
 };
 
 module.exports = PgpWrapper;
