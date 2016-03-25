3a1d4d52d8989e559e1569e2373d62d75c94708b
Jeffrey Mealo <jeffreymealo@gmail.com>
2016-03-25 09:12:25 -0400

    Include introspection in /test endpoint

--- node-services/spark-api/routes/test/index.js
+++ node-services/spark-api/routes/test/index.js
@@ -8,12 +8,13 @@ function *getHandler() {
         headers: ctx.headers,
         query: ctx.query,
         body: ctx.body,
-        guc: yield ctx.pgp.one(ctx.guc(`
+        guc: yield ctx.pgp.any(`
             SELECT current_setting('spark.user_id') AS user_id,
                    current_setting('spark.request_id') AS request_id,
                    current_setting('spark.role') AS role,
                    current_setting('application_name') AS application_name
-        `))
+        `),
+        intospection: ctx.introspection
     };
 }
 
