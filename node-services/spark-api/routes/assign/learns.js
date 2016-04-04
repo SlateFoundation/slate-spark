0719e7a6df07ec013788732b0e0640875598f0a5
Jeffrey Mealo <jeffreymealo@gmail.com>
2016-04-04 17:25:33 -0400

    GET /assign/learns should be enveloped and return learns_required

--- node-services/spark-api/routes/assign/learns.js
+++ node-services/spark-api/routes/assign/learns.js
@@ -6,52 +6,65 @@ var util = require('../../lib/util'),
 
 function *getHandler() {
     var ctx = this,
-        codifyRecord = util.codifyRecord.bind(ctx),
         sectionId = ctx.query.section_id,
         sparkpointId = ctx.query.sparkpoint_id,
-        sql, result;
+        result;
 
     ctx.assert(sectionId, 400, 'section_id, section_code, or section must be passed as a query parameter');
     ctx.assert(sparkpointId, 400, 'sparkpoint_id, sparkpoint_code, or sparkpoint must be passed as a query parameter');
 
-    sql = `
-        WITH assignments AS (
+    result = (yield ctx.pgp.one(`
+        WITH playlist_cache AS (
+            SELECT playlist
+              FROM learn_playlist_cache
+             WHERE sparkpoint_id = $1
+               AND section_id = $2
+          ORDER BY last_updated DESC
+             LIMIT 1
+        ), assignments AS (
             SELECT resource_id,
-                   json_object_agg(coalesce(student_id :: TEXT, 'section'), assignment) AS assignments
+                   json_object_agg(
+                       coalesce(student_id::TEXT, 'section'),
+                       assignment
+                   ) AS assignments
               FROM learn_assignments
              WHERE sparkpoint_id = $1
                AND section_id = $2
           GROUP BY resource_id
+        ), learns_required AS (
+            SELECT json_object_agg(
+                      coalesce(student_id::text, 'section'),
+                      required
+                   ) AS learns_required
+              FROM learns_required
+             WHERE sparkpoint_id = 'M10001CD'
+               AND section_id = 3
         )
-
-        SELECT (
-                 SELECT playlist
-                   FROM learn_playlist_cache
-                  WHERE sparkpoint_id = $1
-                    AND section_id = $2
-               ORDER BY last_updated DESC
-                  LIMIT 1
-               ) AS playlist_cache,
-               COALESCE((
-                 SELECT json_object_agg(resource_id, assignments)
-                   FROM assignments
-               ), '{}'::JSON) AS assignments;
-   `;
-
-    result = yield ctx.pgp.one(sql, [sparkpointId, sectionId]);
+        
+        SELECT json_build_object(
+            'resources',
+            (SELECT playlist FROM playlist_cache),
+            'assignments',
+            coalesce((SELECT json_object_agg(resource_id, assignments) FROM assignments), '{}'::JSON),
+            'learns_required',
+            coalesce((SELECT learns_required FROM learns_required), '{}'::JSON)
+        ) AS json;
+    `, [sparkpointId, sectionId])).json;
 
     ctx.assert(
-        Array.isArray(result.playlist_cache),
+        Array.isArray(result.resources),
         404,
         `No playlist cache found for section_id: ${sectionId} sparkpoint_id: ${sparkpointId}`
     );
 
-    result.playlist_cache.forEach(function(resource) {
+    result.resources.forEach(function(resource) {
         resource.assignments = result.assignments[resource.resource_id] || {};
         delete resource.assignment;
     });
 
-    ctx.body = result.playlist_cache;
+    result.learns_required.site = 5;
+
+    ctx.body = result;
 }
 
 module.exports = {
