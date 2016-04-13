974cddc1016b02a0edbebb625fc318d3308a8bd3
Jeffrey Mealo <jeffreymealo@gmail.com>
2016-04-13 16:19:40 -0400

    /assign/applies should return "work" model compatible applies

--- node-services/spark-api/routes/assign/applies.js
+++ node-services/spark-api/routes/assign/applies.js
@@ -19,17 +19,12 @@ function* getHandler() {
     ctx.assert(standardIds.length > 0, `No academic standards are associated with sparkpoint: ${sparkpointId}`, 404);
     ctx.assert(ctx.isTeacher, 'Only teachers can assign applies', 403);
 
-    result = yield this.pgp.one(
-        //language=SQL
+
+
+    result = yield this.pgp.one(//language=SQL
         `
         WITH applies AS (
-            SELECT fusebox_apply_projects.*,
-                   id AS resource_id,
-                   '' AS creator,
-                   created,
-                   title,
-                   dok,
-                   gradelevel AS "gradeLevel"
+            SELECT *
               FROM fusebox_apply_projects
              WHERE standardids ?| $1
         ),
@@ -50,20 +45,82 @@ function* getHandler() {
                   FROM apply_assignments
                  WHERE sparkpoint_id = $2
                    AND section_id = $3
-                   AND resource_id = ANY(SELECT resource_id FROM applies)
+                   AND resource_id = ANY(SELECT id FROM applies)
              ) t GROUP BY resource_id
         )
-          
+        
         SELECT json_build_object(
             'applies',
             COALESCE((
-              SELECT json_agg(row_to_json(t)) FROM (
-                SELECT
-                  applies.*,
-                  COALESCE(apply_assignments.assignment, '{}'::JSON) AS assignment
-                FROM applies
-                  LEFT JOIN apply_assignments ON apply_assignments.resource_id = applies.resource_id
-              ) t
+              SELECT json_agg(json_build_object(
+                'id',
+                ap.id,
+                'resource_id',
+                ap.id,
+                'created',
+                ap.created,
+                'creator',
+                '', // TODO: Add this to view
+                'title',
+                ap.title,
+                'instructions',
+                ap.instructions,
+                'dok',
+                ap.dok,
+                'gradeLevel',
+                ap.gradelevel,
+                'timeEstimate',
+                ap.timeestimate,
+                'sparkpointIds',
+                (SELECT json_agg(id)
+                 FROM sparkpoints
+                WHERE metadata->>'asn_id' = ANY($1)
+                ),
+                'sparkpointCodes',
+                (SELECT json_agg(code)
+                 FROM sparkpoints
+                WHERE metadata->>'asn_id' = ANY($1)
+                ),
+                'standardCodes',
+                ap.standards,
+                'todos',
+                COALESCE((SELECT json_agg(json_build_object(
+                    'todo',
+                     todo,
+                     'completed',
+                     false
+                )) AS todo
+                FROM jsonb_array_elements(ap.todos) AS todo
+                ), '[]'::JSON),
+                'links',
+                COALESCE((SELECT json_agg(
+                  CASE WHEN jsonb_typeof(link) = 'string'
+                       THEN jsonb_build_object('title', link, 'url', link)
+                       ELSE link
+                  END)
+                 FROM jsonb_array_elements(ap.links) AS link
+                ), '[]'::JSON),
+                'metadata',
+                CASE
+                WHEN ap.metadata = '""'
+                THEN '{}'::JSONB
+                ELSE ap.metadata::JSONB
+                END,
+                'selected',
+                false,
+                'reflection',
+                null,
+                'submissions',
+                '[]'::JSON,
+                'comment',
+                null,
+                'rating',
+                null,
+                'grade',
+                null,
+                'graded_by',
+                null
+            )) FROM applies ap
             ), '[]'::JSON)
         ) AS json;
     `, [standardIds, sparkpointId, sectionId]);
