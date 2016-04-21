746f599812884a2225639276d81ceea7ef39e397
Jeffrey Mealo <jeffreymealo@gmail.com>
2016-04-21 15:02:49 -0400

    cast after lookup

--- node-services/spark-api/routes/assignments/entity.js
+++ node-services/spark-api/routes/assignments/entity.js
@@ -44,14 +44,14 @@ function *sqlGenerator(entity, records, vals) {
     for (let x = 0, len = records.length; x < len; x++) {
         let record = records[x];
 
-        record.student_id = record.student_id || null;
-        record.section_id = record.section_id || null;
-
         try {
             yield util.identifyRecord(record, ctx.lookup);
         } catch (e) {
             ctx.throw(400, e);
         }
+
+        record.student_id = record.student_id || null;
+        record.section_id = record.section_id || null;
 
         if (record.teacher_id === undefined) {
             record.teacher_id = ctx.userId;
