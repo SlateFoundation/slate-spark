11a802bb6b72af9448d6639a38ebe85e0d16bdfd
Jeffrey Mealo <jeffreymealo@gmail.com>
2016-04-21 08:48:33 -0400

    Simplify GET /work/activity endpoint and change this to ctx in PATCH

--- node-services/spark-api/routes/work/activity.js
+++ node-services/spark-api/routes/work/activity.js
@@ -4,65 +4,63 @@ function *getHandler() {
     this.require(['section_id']);
 
     var sectionId = this.query.section_id,
-        ctx = this,
-        query = `
-            SELECT t.last_accessed,
-                   t.section_id,
-                   t.student_id,
-                   t.sparkpoint_id,
-                   t.recommender_id,
-                   t.recommended_time,
-                   learn_start_time,
-                   learn_finish_time,
-                   conference_start_time,
-                   conference_finish_time,
-                   conference_join_time,
-                   apply_start_time,
-                   apply_ready_time,
-                   apply_finish_time,
-                   assess_start_time,
-                   assess_ready_time,
-                   assess_finish_time,
-                   conference_group_id,
-                   selected_apply_id,
-                   selected_fb_apply_id,
-                   learn_mastery_check_score,
-                   conference_mastery_check_score,
-                   code AS sparkpoint
-              FROM (
-                SELECT student_id,
-                       sparkpoint_id,
-                       section_id,
-                       last_accessed,
-                       recommender_id,
-                       recommended_time,
-                       ROW_NUMBER() OVER (
-                         PARTITION BY ssas.student_id, ssas.section_id
-                             ORDER BY ssas.last_accessed DESC) AS rn
-                  FROM section_student_active_sparkpoint ssas
-                 WHERE section_id = $1 AND last_accessed IS NOT NULL
-              ) t
-         LEFT JOIN student_sparkpoint ss ON ss.sparkpoint_id = t.sparkpoint_id
-               AND ss.student_id = t.student_id
-              JOIN sparkpoints ON sparkpoints.id = t.sparkpoint_id
-             WHERE t.rn = 1;`,
-        records;
+        ctx = this;
 
-    records = yield this.pgp.manyOrNone(query, [sectionId]);
-
-    records.forEach(function(record) {
-        record.section_code = ctx.lookup.section.cache.idToCode[record.section_id];
-    });
-
-    this.body = records;
+    ctx.body = yield ctx.pgp.manyOrNone(/*language=SQL*/ `
+        SELECT t.last_accessed,
+               t.section_id,
+               t.section_code,
+               t.student_id,
+               t.sparkpoint_id,
+               t.recommender_id,
+               t.recommended_time,
+               learn_start_time,
+               learn_finish_time,
+               conference_start_time,
+               conference_finish_time,
+               conference_join_time,
+               apply_start_time,
+               apply_ready_time,
+               apply_finish_time,
+               assess_start_time,
+               assess_ready_time,
+               assess_finish_time,
+               conference_group_id,
+               selected_apply_id,
+               selected_fb_apply_id,
+               learn_mastery_check_score,
+               conference_mastery_check_score,
+               code AS sparkpoint
+          FROM (
+            SELECT student_id,
+                   sparkpoint_id,
+                   section_id,
+                   last_accessed,
+                   recommender_id,
+                   recommended_time,
+                   cs."Code" AS section_code,
+                   ROW_NUMBER() OVER (
+                     PARTITION BY ssas.student_id, ssas.section_id
+                         ORDER BY ssas.last_accessed DESC) AS rn
+              FROM section_student_active_sparkpoint ssas
+         LEFT JOIN course_sections cs ON cs."ID" = section_id
+             WHERE section_id = $1 AND last_accessed IS NOT NULL
+          ) t
+     LEFT JOIN student_sparkpoint ss ON ss.sparkpoint_id = t.sparkpoint_id
+           AND ss.student_id = t.student_id
+          JOIN sparkpoints ON sparkpoints.id = t.sparkpoint_id
+         WHERE t.rn = 1;`,
+        [sectionId]
+    );
 }
 
 function *patchHandler(req, res, next) {
     this.require(['student_id', 'section_id', 'sparkpoint_id']);
 
-    var sectionId = this.query.section_id,
-        studentId = this.studentId,
-        sparkpointId = this.query.sparkpoint_id,
+    var ctx = this,
+        sectionId = ctx.query.section_id,
+        studentId = ctx.studentId,
+        sparkpointId = ctx.query.sparkpoint_id,
         allowedKeys = [
             'sparkpoint',
             'learn_start_time',
@@ -83,9 +81,8 @@ function *patchHandler(req, res, next) {
         timeKeys = [],
         updateValues = [],
         timeValues = [],
-        body = this.request.body,
+        body = ctx.request.body,
         allKeys = Object.keys(body || {}),
-        ctx = this,
         hasMasteryCheckScores = false,
         invalidKeys, activeSql, sparkpointSql, record, values;
 
@@ -159,27 +156,27 @@ function *patchHandler(req, res, next) {
     });
 
     if (invalidKeys.length > 0) {
-        this.throw(new Error('Unexpected field(s) encountered: ' + invalidKeys.join(', ')), 400);
+        ctx.throw(new Error('Unexpected field(s) encountered: ' + invalidKeys.join(', ')), 400);
     }
 
     activeSql = `
         INSERT INTO section_student_active_sparkpoint
-                    (section_id, student_id, sparkpoint_id, ${this.isStudent ? 'last_accessed' : 'recommended_time, recommender_id'})
-             VALUES ($1, $2, $3, now()::timestamp without time zone ${this.isStudent ? '' : ', $4'})
+                    (section_id, student_id, sparkpoint_id, ${ctx.isStudent ? 'last_accessed' : 'recommended_time, recommender_id'})
+             VALUES ($1, $2, $3, now()::timestamp without time zone ${ctx.isStudent ? '' : ', $4'})
         ON CONFLICT (section_id, student_id, sparkpoint_id) DO `;
 
     values = [sectionId, studentId, sparkpointId];
 
-    if (this.isTeacher) {
+    if (ctx.isTeacher) {
         activeSql += 'UPDATE SET recommender_id = $4, recommended_time = now()::timestamp without time zone;';
-        values.push(this.userId);
+        values.push(ctx.userId);
     } else {
         activeSql += 'UPDATE SET last_accessed = now()::timestamp without time zone;';
     }
 
     if (!hasMasteryCheckScores) {
         // Do not set the recommended time when a teacher posts a mastery check score
-        yield this.pgp.oneOrNone(activeSql, values);
+        yield ctx.pgp.oneOrNone(activeSql, values);
     }
 
     if (timeKeys.length === 0) {
@@ -191,7 +188,7 @@ function *patchHandler(req, res, next) {
                AND sparkpoint_id = $2;
         `;
 
-        record = yield this.pgp.oneOrNone(sparkpointSql, [studentId, sparkpointId]);
+        record = yield ctx.pgp.oneOrNone(sparkpointSql, [studentId, sparkpointId]);
 
         if (!record) {
             record = {
@@ -215,16 +212,16 @@ function *patchHandler(req, res, next) {
                              DO UPDATE SET ${updateValues.join(',\n')}
                                  RETURNING *;`;
 
-        record = yield this.pgp.one(sparkpointSql, [studentId, sparkpointId]);
+        record = yield ctx.pgp.one(sparkpointSql, [studentId, sparkpointId]);
     }
 
     delete record.id;
-    record.section_id = this.query.section_id;
-    record = util.codifyRecord(record, this.lookup);
+    record.section_id = ctx.query.section_id;
+    record = util.codifyRecord(record, ctx.lookup);
     // TODO: Deprecate sparkpoint (use sparkpoint_code instead)
     record.sparkpoint = record.sparkpoint_code;
 
-    this.body = record;
+    ctx.body = record;
 }
 
 module.exports = {
