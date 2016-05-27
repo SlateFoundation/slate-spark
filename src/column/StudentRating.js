a4ad761c8699d90ec811b6a4328a5191d1d1ca34
Ryon Coleman <ryon55@gmail.com>
2016-05-27 00:07:50 -0400

    Redesign StudentRating column

--- src/column/StudentRating.js
+++ src/column/StudentRating.js
@@ -1,7 +1,7 @@
 Ext.define('SparkClassroom.column.StudentRating', {
     extend: 'Ext.grid.column.Column',
     xtype: 'spark-column-studentrating',
-
+	cls: 'spark-column-studentrating',
 
     config: {
         enableEditing: false,
@@ -10,31 +10,21 @@ Ext.define('SparkClassroom.column.StudentRating', {
         width: 112,
         text: 'Your Rating',
         cell: {
+	        cls: 'spark-cell-studentrating',
             encodeHtml: false
         },
         renderer: function(v, r) {
-            var out = [],
-                n = 10;
+            return [
+            	'<button type="button" class="plain spark-studentrating-like">',
+            		'<i class="fa fa-thumbs-up"></i>',
+            		'<span class="visually-hidden">Like</span>',
+            	'</button>',
 
-            v = Ext.isObject(v) ? v.user : v;
-
-            if (this.getEnableEditing()) {
-                out.push('<select class="field-control">');
-
-                if (!v) {
-                    out.push('<option></option>');
-                }
-
-                for (; n > 0; n--) {
-                    out.push('<option'+(n == v ? ' selected' : '')+'>'+n+'</option>');
-                }
-
-                out.push('</select>');
-
-                return out.join('');
-            }
-
-            return v;
+            	'<button type="button" class="plain spark-studentrating-dislike">',
+            		'<i class="fa fa-thumbs-down"></i>',
+            		'<span class="visually-hidden">Dislike</span>',
+            	'</button>',
+            ].join(' ');
         }
     }
 });
