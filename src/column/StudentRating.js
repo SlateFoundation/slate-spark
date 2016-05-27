8372dffc878bbe5cc62b86f5d158e72b8ee9ddde
Ryon Coleman <ryon55@gmail.com>
2016-05-27 00:46:25 -0400

    Add dummy conditionals to student rating column, for selected/disabled states

--- src/column/StudentRating.js
+++ src/column/StudentRating.js
@@ -1,7 +1,7 @@
 Ext.define('SparkClassroom.column.StudentRating', {
     extend: 'Ext.grid.column.Column',
     xtype: 'spark-column-studentrating',
-	cls: 'spark-column-studentrating',
+    cls: 'spark-column-studentrating',
 
     config: {
         enableEditing: false,
@@ -10,21 +10,26 @@ Ext.define('SparkClassroom.column.StudentRating', {
         width: 112,
         text: 'Your Rating',
         cell: {
-	        cls: 'spark-cell-studentrating',
-            encodeHtml: false
+            cls: 'spark-cell-studentrating',
+            encodeHtml: false,
         },
-        renderer: function(v, r) {
-            return [
-            	'<button type="button" class="plain spark-studentrating-like">',
-            		'<i class="fa fa-thumbs-up"></i>',
-            		'<span class="visually-hidden">Like</span>',
-            	'</button>',
+        tpl: [
+            // TODO use correct conditionals for .is-selected and [disabled] states
+            '<button type="button"',
+                'class="plain spark-studentrating-like <tpl if="like">is-selected</tpl>"',
+                '<tpl if="readonly">disabled</tpl>',
+            '>',
+                '<i class="fa fa-thumbs-up"></i>',
+                '<span class="visually-hidden">Like</span>',
+            '</button>',
 
-            	'<button type="button" class="plain spark-studentrating-dislike">',
-            		'<i class="fa fa-thumbs-down"></i>',
-            		'<span class="visually-hidden">Dislike</span>',
-            	'</button>',
-            ].join(' ');
-        }
+            '<button type="button"',
+                'class="plain spark-studentrating-dislike <tpl if="dislike">is-selected</tpl>"',
+                '<tpl if="!!!readonly">disabled</tpl>',
+            '>',
+                '<i class="fa fa-thumbs-down"></i>',
+                '<span class="visually-hidden">Dislike</span>',
+            '</button>',
+        ]
     }
 });
