5a0baaa43f15e05fc34fadadd02788b7188d0793
Nafis Bey <nafis@jarv.us>
2016-05-27 23:16:41 -0400

    add fields for competencies grid sorting & identification

--- src/model/Activity.js
+++ src/model/Activity.js
@@ -1,25 +1,37 @@
 /*jslint browser: true, undef: true *//*global Ext*/
 Ext.define('SparkClassroom.model.Activity', {
     extend: 'Ext.data.Model',
+    // extend: 'SparkClassroom.model.StudentSparkpoint',
+    requires: [
+        'Slate.proxy.API'
+    ],
+
+    idProperty: 'student_sparkpointid',
 
     fields: [
-        'sparkpoint_id',
-        'sparkpoint',
-
         'section_id',
-        'section_code'
+        'section_code',
+
+        'sparkpoint_id',
+        'student_id',
+
+        {
+            name: 'student_sparkpointid',
+            depends: ['student_id', 'sparkpoint_id'],
+            persist: false,
+            crtical: true,
+            convert: function(v, r) {
+                return r.get('student_id') + '_' + r.get('sparkpoint_id');
+            }
+        }
     ],
 
     proxy: {
         type: 'slate-api',
         url: '/spark/api/work/activity',
+
         extraParams: {
             status: 'all'
-        },
-        reader: {
-            type: 'json',
-            rootProperty: null,
-            keepRawData: true
         }
     }
 });
