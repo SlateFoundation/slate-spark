54d60f0462b09b289686efda2ce01dde95ddf91d
Nafis Bey <nafis@jarv.us>
2016-06-14 12:50:30 -0400

    Remove Competencies Store reference

--- sencha-workspace/SparkClassroomTeacher/app/controller/Competencies.js
+++ sencha-workspace/SparkClassroomTeacher/app/controller/Competencies.js
@@ -15,9 +15,7 @@ Ext.define('SparkClassroomTeacher.controller.Competencies', {
     ],
 
     stores: [
-        'Competencies',
-        'Activities@SparkClassroom.store',
-
+        'Activities@SparkClassroom.store'
     ],
 
     refs:{
