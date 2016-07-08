863a95a148a4869360f139f5f40cc422eeb29aee
Ryon Coleman <ryon55@gmail.com>
2016-07-08 02:17:57 -0400

    Add "+Q" button to Sparkpoints column

--- src/column/Sparkpoints.js
+++ src/column/Sparkpoints.js
@@ -6,6 +6,18 @@ Ext.define('SparkClassroom.column.Sparkpoints', {
         dataIndex: 'sparkpoint',
         cls: 'spark-sparkpoints-column',
         width: 208,
-        text: 'Sparkpoints'
+        text: 'Sparkpoints',
+        cell: {
+            cls: 'spark-sparkpoints-cell',
+            encodeHtml: false,
+        },
+        renderer: function (value, record) {
+            return [
+                '<div class="flex-ct">',
+                    '<span class="spark-column-value flex-1">', value, '</span>',
+                    '<button class="button tiny" action="add-to-queue">+&nbsp;Q</button>',
+                '</div>'
+            ].join('');
+        }
     }
 });
