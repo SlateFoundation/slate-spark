bc18733b8de7bb4b27289224d82014f2406d07c5
Chris Alfano <chris@jarv.us>
2016-08-03 10:23:02 -0400

    Add git layer configuration

---
+++ php-config/Git.config.d/spark-fusebox.php
@@ -0,0 +1,24 @@
+<?php
+
+Git::$repositories['spark-fusebox'] = [
+    'remote' => 'git@github.com:JarvusInnovations/spark-fusebox.git',
+    'originBranch' => 'develop',
+    'workingBranch' => 'develop',
+    'localOnly' => true,
+    'trees' => [
+        'html-templates/app/SparkRepositoryManager',
+        'php-classes/Spark3',
+        'php-classes/Spark2',
+        'php-classes/JSON.class.php', // TODO: merge upstream
+        'php-classes/PostgresPDO.php', // TODO: move into a namespace and merge upstream
+        'php-config/Git.config.d/spark-fusebox.php',
+        'sencha-workspace/SparkRepositoryManager',
+        'sencha-workspace/packages/saki-grid-multisearch-5',
+        'sencha-workspace/packages/spark-manager-theme',
+        'site-root/spark2',
+        'site-root/spark-repo',
+        'site-root/postgrest.php',
+        'site-root/sass',
+        'site-root/fonts'
+    ]
+];
