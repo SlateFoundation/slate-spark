2461188d59205ced4d532974b5187d32253bd865
Ryon Coleman <ryon55@gmail.com>
2016-08-03 23:41:18 -0400

    Add multiselector to unit learns editor [WIP]

--- sencha-workspace/SparkRepositoryManager/app/view/units/editor/Learn.js
+++ sencha-workspace/SparkRepositoryManager/app/view/units/editor/Learn.js
@@ -5,5 +5,42 @@ Ext.define('SparkRepositoryManager.view.units.editor.Learn', {
     title: 'Learn &amp; Practice',
     componentCls: 's2m-units-editor-intro',
 
-    html: 'Learn'
+    items: [
+        {
+            xtype: 'multiselector',
+
+            fieldName: 'title',
+            fieldTitle: 'Learn Title',
+            store: {
+                fields: [
+                    'sparkpoint',
+                    'title',
+                    'url',
+                    {
+                        name: 'required',
+                        type: 'boolean',
+                        defaultValue: false
+                    },
+                    {
+                        name: 'recommended',
+                        type: 'boolean',
+                        defaultValue: false
+                    }
+                ],
+
+                data: [
+                    {
+                        sparkpoint: 'SS.G6.1.2.A',
+                        title: 'Genetic Drift',
+                        url: 'http://example.com'
+                    },
+                    {
+                        sparkpoint: 'SS.G6.1.2.A',
+                        title: 'Learn Biology: Trophic Levels and Producer vs. Consumer',
+                        url: 'http://example.com'
+                    }
+                ]
+            }
+        }
+    ]
 });
