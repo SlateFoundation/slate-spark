62db7368e60e80e3b5578193a80fb0fd397a9a7c
rgipson <ryangipson@gmail.com>
2016-08-09 21:07:25 -0400

    Add calculated fields for completed times in Activity model

--- src/model/Activity.js
+++ src/model/Activity.js
@@ -11,19 +11,23 @@ Ext.define('SparkClassroom.model.Activity', {
             name: 'completed_phase_numerical',
             depends: [
                 'learn_finish_time',
+                'learn_override_time',
                 'conference_finish_time',
+                'conference_override_time',
                 'apply_finish_time',
-                'assess_finish_time'
+                'apply_override_time',
+                'assess_finish_time',
+                'assess_override_time'
             ],
             persist: false,
             calculate: function(data) {
-                if (data.assess_finish_time) {
+                if (data.assess_finish_time || data.assess_override_time) {
                     return 4;
-                } else if (data.apply_finish_time) {
+                } else if (data.apply_finish_time || data.apply_override_time) {
                     return 3;
-                } else if (data.conference_finish_time) {
+                } else if (data.conference_finish_time || data.conference_override_time) {
                     return 2;
-                } else if (data.learn_finish_time) {
+                } else if (data.learn_finish_time || data.learn_override_time) {
                     return 1;
                 } else {
                     return 0;
@@ -37,6 +41,54 @@ Ext.define('SparkClassroom.model.Activity', {
             critical: true,
             calculate: function(data) {
                 return data.student_id + '_' + data.sparkpoint_id;
+            }
+        },
+
+        {
+            name: 'learn_completed_time',
+            depends: [
+                'learn_finish_time',
+                'learn_override_time'
+            ],
+            persist: false,
+            calculate: function(data) {
+                return data.learn_finish_time || data.learn_override_time || null
+            }
+        },
+
+        {
+            name: 'conference_completed_time',
+            depends: [
+                'conference_finish_time',
+                'conference_override_time'
+            ],
+            persist: false,
+            calculate: function(data) {
+                return data.conference_finish_time || data.conference_override_time || null
+            }
+        },
+
+        {
+            name: 'apply_completed_time',
+            depends: [
+                'apply_finish_time',
+                'apply_override_time'
+            ],
+            persist: false,
+            calculate: function(data) {
+                return data.apply_finish_time || data.apply_override_time || null
+            }
+        },
+
+        {
+            name: 'assess_completed_time',
+            depends: [
+                'assess_finish_time',
+                'assess_override_time'
+            ],
+            persist: false,
+            calculate: function(data) {
+                return data.assess_finish_time || data.assess_override_time || null
             }
         }
     ],
