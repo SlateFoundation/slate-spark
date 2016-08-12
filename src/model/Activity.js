be3d6200c8507666bccf438e12e8f4a1ac9f314b
rgipson <ryangipson@gmail.com>
2016-08-11 20:28:59 -0400

    Add improvements to code practice

--- src/model/Activity.js
+++ src/model/Activity.js
@@ -8,30 +8,23 @@ Ext.define('SparkClassroom.model.Activity', {
         'section_id',
         'section_code',
         {
-            name: 'completed_phase_numerical',
-            depends: [
-                'learn_finish_time',
-                'learn_override_time',
-                'conference_finish_time',
-                'conference_override_time',
-                'apply_finish_time',
-                'apply_override_time',
-                'assess_finish_time',
-                'assess_override_time'
-            ],
+            name: 'completed_phase_number',
             persist: false,
             calculate: function(data) {
                 if (data.assess_finish_time || data.assess_override_time) {
                     return 4;
-                } else if (data.apply_finish_time || data.apply_override_time) {
-                    return 3;
-                } else if (data.conference_finish_time || data.conference_override_time) {
-                    return 2;
-                } else if (data.learn_finish_time || data.learn_override_time) {
-                    return 1;
-                } else {
-                    return 0;
                 }
+                if (data.apply_finish_time || data.apply_override_time) {
+                    return 3;
+                }
+                if (data.conference_finish_time || data.conference_override_time) {
+                    return 2;
+                }
+                if (data.learn_finish_time || data.learn_override_time) {
+                    return 1;
+                }
+
+                return 0;
             }
         },
 
@@ -45,10 +38,6 @@ Ext.define('SparkClassroom.model.Activity', {
 
         {
             name: 'learn_completed_time',
-            depends: [
-                'learn_finish_time',
-                'learn_override_time'
-            ],
             persist: false,
             calculate: function(data) {
                 return data.learn_finish_time || data.learn_override_time || null
@@ -57,10 +46,6 @@ Ext.define('SparkClassroom.model.Activity', {
 
         {
             name: 'conference_completed_time',
-            depends: [
-                'conference_finish_time',
-                'conference_override_time'
-            ],
             persist: false,
             calculate: function(data) {
                 return data.conference_finish_time || data.conference_override_time || null
@@ -69,10 +54,6 @@ Ext.define('SparkClassroom.model.Activity', {
 
         {
             name: 'apply_completed_time',
-            depends: [
-                'apply_finish_time',
-                'apply_override_time'
-            ],
             persist: false,
             calculate: function(data) {
                 return data.apply_finish_time || data.apply_override_time || null
@@ -81,10 +62,6 @@ Ext.define('SparkClassroom.model.Activity', {
 
         {
             name: 'assess_completed_time',
-            depends: [
-                'assess_finish_time',
-                'assess_override_time'
-            ],
             persist: false,
             calculate: function(data) {
                 return data.assess_finish_time || data.assess_override_time || null
