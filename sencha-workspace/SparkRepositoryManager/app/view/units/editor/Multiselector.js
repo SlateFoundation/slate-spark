9b10ec9df93ad9f69ef5c805d4b29f1ba735255e
Ryon Coleman <ryon55@gmail.com>
2016-08-11 20:53:16 -0400

    Add base Multiselector component for units editors

---
+++ sencha-workspace/SparkRepositoryManager/app/view/units/editor/Multiselector.js
@@ -0,0 +1,172 @@
+Ext.define('SparkRepositoryManager.view.units.editor.Multiselector', {
+    extend: 'Ext.Container',
+    xtype: 's2m-units-multiselector',
+
+    componentCls: 's2m-units-multiselector',
+
+    config: {
+        itemType: {
+            singular: 'Item',
+            plural: 'Items'
+        },
+        sparkpointsStore: null,
+        unitHeaderItems: null,
+        unitStore: null,
+        showRequired: false,
+        showRecommended: false
+    },
+
+    layout: {
+        type: 'hbox',
+        align: 'stretch'
+    },
+    items: [
+        {
+            flex: 1,
+            itemId: 'sparkpoints-grid',
+            xtype: 'grid',
+            features: [{
+                ftype: 'grouping',
+                groupHeaderTpl: '{name}',
+                collapsible: false // disabled because it doesn't work TODO maybe make this work?,
+            }],
+            hideHeaders: true,
+            disableSelection: true,
+            columns: [
+                {
+                    xtype: 'templatecolumn',
+                    flex: 1,
+                    cellWrap: true,
+                    tpl: [
+                        '<tpl if="url">',
+                            '<a href="{url}" target="_blank">{title}</a>',
+                        '<tpl else>',
+                            '{title}',
+                        '</tpl>'
+                    ]
+                },
+                {
+                    xtype: 'actioncolumn',
+                    width: 24,
+                    items: [
+                        {
+                            action: 'add',
+                            iconCls: 'glyph-success',
+                            tooltip: 'Add to Unit',
+                            glyph: 0xf055 // fa-plus-circle
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            flex: 2,
+            itemId: 'unit-grid',
+            xtype: 'grid',
+            cls: 's2m-units-multiselector-unitgrid',
+            features: [{
+                ftype: 'grouping',
+                groupHeaderTpl: '{name}',
+                collapsible: false // disabled because it doesn't work TODO maybe make this work?,
+            }],
+            viewConfig: {
+                plugins: {
+                    ptype: 'gridviewdragdrop'
+                }
+            },
+            hideHeaders: true,
+            columns: {
+                defaults: {
+                    menuDisabled: true
+                },
+                items: [
+                    {
+                        width: 24,
+                        xtype: 'actioncolumn',
+                        align: 'right',
+                        tdCls: 's2m-units-remove-cell',
+                        items: [
+                            {
+                                action: 'remove',
+                                iconCls: 'glyph-danger',
+                                tooltip: 'Remove from Unit',
+                                glyph: 0xf056 // fa-minus-circle
+                            }
+                        ]
+                    },
+                    {
+                        width: 32,
+                        xtype: 'rownumberer',
+                        draggable: true // TODO does this work? interfere?
+                    },
+                    {
+                        flex: 1,
+                        xtype: 'templatecolumn',
+                        itemId: 'unit-item-col',
+                        cellWrap: true,
+                        tpl: [
+                            '<tpl if="url">',
+                                '<a href="{url}" target="_blank">{title}</a>',
+                            '<tpl else>',
+                                '{title}',
+                            '</tpl>'
+                        ]
+                    },
+                    {
+                        width: 80,
+                        xtype: 'checkcolumn',
+                        itemId: 'unit-required-column',
+                        text: '<abbr title="Required">Req’d</abbr>',
+                        dataIndex: 'isRequired',
+                        hidden: true
+                    },
+                    {
+                        width: 96,
+                        xtype: 'checkcolumn',
+                        itemId: 'unit-recommended-column',
+                        text: '<abbr title="Recommended">Recm’d</abbr>',
+                        dataIndex: 'isRecommended',
+                        hidden: true
+                    }
+                ]
+            }
+        }
+    ],
+
+    afterRender: function() {
+        var me = this,
+            itemType = me.getItemType(),
+            sparkpointsStore = me.getSparkpointsStore(),
+            sparkpointsGrid = me.down('#sparkpoints-grid'),
+            unitStore = me.getUnitStore(),
+            unitGrid = me.down('#unit-grid'),
+            unitHeaderItems = me.getUnitHeaderItems(),
+            showRequired = me.getShowRequired(),
+            showRecommended = me.getShowRecommended();
+
+        me.callParent();
+
+        sparkpointsGrid.setTitle(itemType.plural + ' in Sparkpoints')
+        sparkpointsGrid.setStore(sparkpointsStore);
+
+        unitGrid.setTitle(itemType.plural + ' in Unit');
+        unitGrid.setStore(unitStore);
+        unitGrid.down('#unit-item-col').setText(itemType.singular);
+
+        if (unitHeaderItems) {
+            unitGrid.getHeader().add(unitHeaderItems);
+            unitGrid.getHeader().setStyle({
+                height: '38px',
+                padding: '6px 11px'
+            });
+        }
+
+        if (showRequired || showRecommended) {
+            unitGrid.hideHeaders = false;
+            unitGrid.getHeaderContainer().setHeight('auto');
+
+            unitGrid.down('#unit-required-column').setVisible(showRequired);
+            unitGrid.down('#unit-recommended-column').setVisible(showRecommended);
+        }
+    }
+});
