69d8f9beb4623339145b5c6437598b0b76f45efe
Jeffrey Mealo <jeffreymealo@gmail.com>
2016-09-12 05:35:17 -0400

    change 403 -> 400

--- node-services/spark-api/routes/work/activity.js
+++ node-services/spark-api/routes/work/activity.js
@@ -232,7 +232,7 @@ function *patchHandler() {
     record.student_id = studentId;
     errors = ctx.validation.student_sparkpoint(record);
 
-    ctx.assert(errors, errors, 403);
+    ctx.assert(errors, errors, 400);
     let result = yield ctx.pgp.oneOrNone(recordToUpsert('student_sparkpoint', record, vals, ['sparkpoint_id', 'student_id']) + ' RETURNING *;', vals.vals);
     ctx.body = result || (yield ctx.pgp.one('SELECT * FROM student_sparkpoint WHERE sparkpoint_id = $1 AND student_id = $2', [sparkpointId, studentId]));
 }
