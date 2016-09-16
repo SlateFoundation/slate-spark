981ce2aedf8daee47b0fc46dc94edde972118d2c
Jeffrey Mealo <jeffreymealo@gmail.com>
2016-09-16 12:03:58 -0400

    remove deprecated sync lookup functions from blocked endpoint

--- node-services/spark-api/routes/work/blocked.js
+++ node-services/spark-api/routes/work/blocked.js
@@ -1,22 +1,20 @@
 'use strict';
 
-var util = require('../../lib/util.js');
-
 function *getHandler() {
     var ctx = this,
-        sectionId = ctx.query.section_id,
-        records;
+        sectionId = ctx.query.section_id;
 
     ctx.assert(ctx.isTeacher, 'You must be a teacher to use this endpoint', 403);
     ctx.assert(sectionId, 'You must provide a section, section_code or section_id as a query parameter', 400);
 
-    records = yield ctx.pgp.manyOrNone(/*language=SQL*/ `
+    ctx.body = yield ctx.pgp.manyOrNone(/*language=SQL*/ `
         SELECT ssas.last_accessed,
                ssas.section_id,
                ssas.student_id,
                ssas.sparkpoint_id,
                ssas.recommender_id,
                ssas.recommended_time,
+                             
                learn_start_time,
                learn_finish_time,
                conference_start_time,
@@ -33,11 +31,14 @@ function *getHandler() {
                selected_apply_resource_id,
                learn_mastery_check_score,
                conference_mastery_check_score,
-               code AS sparkpoint
+               
+               s.code AS sparkpoint,
+               cs."Code" AS section_code
           FROM section_student_active_sparkpoint ssas
           JOIN student_sparkpoint ss ON ss.student_id = ssas.student_id
            AND ss.sparkpoint_id = ssas.sparkpoint_id
           JOIN sparkpoints s ON s.id = ssas.sparkpoint_id
+          JOIN course_sections cs ON cs."ID" = ssas.section_id
          WHERE section_id = $1
            AND ssas.last_accessed IS NOT NULL
            AND (
@@ -48,12 +49,6 @@ function *getHandler() {
            )`,
         [sectionId]
     );
-
-    records.forEach(function(record) {
-        record.section_code = ctx.lookup.section.cache.idToCode[record.section_id];
-    });
-
-    ctx.body = records;
 }
 
 module.exports = {
