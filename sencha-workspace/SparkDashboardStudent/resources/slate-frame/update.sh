91f82ddc160e4f778a67cfc6bfa90aebbb82327e
Ryon Coleman <ryon55@gmail.com>
2016-09-21 05:00:17 -0400

    Add slate-frame to student dashboard app

---
+++ sencha-workspace/SparkDashboardStudent/resources/slate-frame/update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+wget https://sandbox-school.matchbooklearning.com/css/main.css -O main.css
+
+mkdir img
+wget https://sandbox-school.matchbooklearning.com/img/slate-logo-white.svg -O img/slate-logo-white.svg
+wget https://sandbox-school.matchbooklearning.com/img/slate-icons/slate-icons.svg -O img/slate-icons.svg
+
+mkdir fonts
+wget https://sandbox-school.matchbooklearning.com/css/fonts/lato.css -O fonts/lato.css
+wget https://sandbox-school.matchbooklearning.com/css/fonts/sanchez.css -O fonts/sanchez.css
+wget https://sandbox-school.matchbooklearning.com/css/fonts/font-awesome.css -O fonts/font-awesome.css
