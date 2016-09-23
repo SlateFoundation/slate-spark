dc4f53d5933d316895d7caf7057e82d57b2e8cae
Jeffrey Mealo <jeffreymealo@gmail.com>
2016-09-23 13:59:29 -0400

    use absolute path for action

--- node-services/spark-api/routes/system/index.js
+++ node-services/spark-api/routes/system/index.js
@@ -3,7 +3,9 @@
 function *getHandler() {
     var ctx = this;
     ctx.type = 'html';
-    ctx.body = '<form action="sync_fusebox_users" method="POST"><button type="submit">Sync Fusebox Users</button></form>';
+    ctx.body = `<form action="/spark/api/system/sync_fusebox_users" method="POST">
+                    <button type="submit">Sync Fusebox Users</button>
+                </form>`;
 }
 
 module.exports = {
