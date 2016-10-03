a297f1bfb7b5e53d954d488370c6026ca0cd0f53
Jeffrey Mealo <jeffreymealo@gmail.com>
2016-10-03 14:02:18 -0400

    Fix regression where section code was not included in suggested sparkpoints

--- node-services/spark-api/routes/sparkpoints/suggested.js
+++ node-services/spark-api/routes/sparkpoints/suggested.js
@@ -12,85 +12,90 @@ function* getHandler() {
     ctx.assert(sectionId > 0, 'section_id is required', 400);
     ctx.assert(studentId > 0, 'Non-student users must provide a student_id', 400);
 
-    results = yield ctx.pgp.one(/*language=SQL*/ `
-    WITH past AS (
-            SELECT sp.id,
-                   sp.code,
-                   sp.student_title,
-                   
-                   ss.*,
-                   
+    results = yield ctx.pgp.any(/*language=SQL*/ `
+        WITH past AS (
+            SELECT ss.*,
+    
+                   ssas.sparkpoint_id AS id,
                    ssas.last_accessed,
                    ssas.section_id,
                    ssas.recommender_id,
                    ssas.recommended_time
-                   
+    
               FROM section_student_active_sparkpoint ssas
          LEFT JOIN student_sparkpoint ss ON ss.sparkpoint_id = ssas.sparkpoint_id
                AND ss.student_id = $1
-              JOIN sparkpoints sp ON sp.id = ssas.sparkpoint_id
              WHERE ssas.section_id = $2
                AND ss.apply_finish_time IS NOT NULL
                AND ssas.student_id = $1
           ORDER BY ssas.last_accessed DESC
              LIMIT $3
          ),
-
+    
          current AS (
-            SELECT sp.id,
-                   sp.code,
-                   sp.student_title,
-                   
-                   ss.*,
-                   
+            SELECT ss.*,
+    
+                   ssas.sparkpoint_id AS id,
                    ssas.last_accessed,
                    ssas.section_id,
                    ssas.recommender_id,
                    ssas.recommended_time
-                   
+    
               FROM section_student_active_sparkpoint ssas
         RIGHT JOIN student_sparkpoint ss ON ss.sparkpoint_id = ssas.sparkpoint_id
                AND ss.student_id = $1
-              JOIN sparkpoints sp ON sp.id = ssas.sparkpoint_id
              WHERE ssas.section_id = $2
                AND ssas.student_id = $1
                AND ss.apply_finish_time IS NULL
           ORDER BY ssas.last_accessed DESC
              LIMIT $4
          ),
-
+    
         queued AS (
-            SELECT sp.id,
-                   sp.code,
-                   sp.student_title,
-                   
-                   ss.*,
-                   
+            SELECT ss.*,
+    
+                   ssas.sparkpoint_id AS id,
                    ssas.last_accessed,
                    ssas.section_id,
                    ssas.recommender_id,
                    ssas.recommended_time
-                   
+    
               FROM section_student_active_sparkpoint ssas
          LEFT JOIN student_sparkpoint ss ON ss.sparkpoint_id = ssas.sparkpoint_id
                AND ss.student_id = $1
-              JOIN sparkpoints sp ON sp.id = ssas.sparkpoint_id
              WHERE ssas.section_id = $2
                AND ssas.student_id = $1
                AND ss.learn_start_time IS NULL
           ORDER BY ssas.recommended_time DESC, ssas.last_accessed DESC
            LIMIT $5
+       ),
+    
+       combined_sparkpoints AS (
+        SELECT * FROM queued
+        UNION DISTINCT
+        SELECT * FROM current
+        UNION DISTINCT
+        SELECT * FROM past
        )
+    
+    
+    SELECT combined_sparkpoints.*,
+           sp.id,
+           sp.code,
+           sp.student_title,
+           cs."Code" AS section
+      FROM combined_sparkpoints
+      JOIN course_sections cs ON cs."ID" = combined_sparkpoints.section_id
+      JOIN sparkpoints sp ON sp.id = combined_sparkpoints.id
+    `, [
+        studentId,
+        sectionId,
+        pastLimit,
+        currentLimit,
+        queuedLimit
+    ]);
 
-       SELECT json_agg(j) AS sparkpoints FROM (
-            SELECT row_to_json(past) j FROM past
-            UNION ALL
-            SELECT row_to_json(current) j FROM current
-            UNION ALL
-            SELECT row_to_json(queued) j FROM queued
-        ) t;`, [ studentId, sectionId, pastLimit, currentLimit, queuedLimit ]);
-
-    ctx.body = results.sparkpoints || [];
+    ctx.body = results || [];
 }
 
 function bustSuggestionCache() {
