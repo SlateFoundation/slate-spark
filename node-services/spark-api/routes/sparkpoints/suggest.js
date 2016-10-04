d6fe91fe173e80f4ecfb64a7264ffe2f694d1f3f
Jeffrey Mealo <jeffreymealo@gmail.com>
2016-10-04 19:15:31 -0400

    Add /sparkpoints/suggest endpoint

---
+++ node-services/spark-api/routes/sparkpoints/suggest.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const identifyRecordSync = require('../../../lib/util').identifyRecordSync;
+const util = require('../../../lib/util');
+
+function *postHandler() {
+    var ctx = this,
+        suggestions = ctx.request.body,
+        sectionId = ~~ctx.query.section_id,
+        records,
+        copyColumns = ['recommended_time', 'student_id', 'sparkpoint_id', 'section_id'],
+        vals = new util.Values(),
+        recordToUpsert = util.recordToUpsert.bind(ctx);
+
+    ctx.assert(ctx.isTeacher, 'Only teachers can use the suggestions endpoint', 403);
+    ctx.assert(Array.isArray(suggestions), 'The suggestions endpoint takes an array of suggestion objects', 400);
+    ctx.assert(sectionId > 0, 'section_id is required', 400);
+
+    records = suggestions.map(function (suggestion) {
+        var record = {};
+
+        suggestion = identifyRecordSync(suggestion, ctx.lookup);
+
+        if (!suggestion.sparkpoint_id && suggestion.section_id && suggestion.student_id) {
+            ctx.throw('sparkpoint_id, section_id and student_id are required', 400);
+        }
+
+        for (var key in suggestion) {
+            if (copyColumns.includes(key)) {
+                record[key] = suggestion[key];
+            } else {
+                ctx.throw(`Unexpected key: ${key}`, 400);
+            }
+        }
+
+        // TODO: We could do additional validation on the timestamps here
+        record.recommended_time = record.recommended_time || new Date().toUTCString();
+        record.recommender_id = ctx.userId;
+
+        return record;
+    });
+
+    records = yield ctx.pgp.any(util.queriesToReturningCte(
+        records.map(record => recordToUpsert(
+            'section_student_active_sparkpoint', record, vals, ['section_id', 'sparkpoint_id', 'student_id']), vals
+        )
+    ), vals.vals);
+
+    ctx.body = records.map(record => util.codifyRecord(record, ctx.lookup));
+}
+
+module.exports = {
+    post: postHandler
+};
