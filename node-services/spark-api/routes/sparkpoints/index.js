f54a42d3662963e173a8a82c8dff8a64e58477f6
Jeffrey Mealo <jeffreymealo@gmail.com>
2016-10-06 16:54:58 -0400

    do not send null student_id and section_id

--- node-services/spark-api/routes/sparkpoints/index.js
+++ node-services/spark-api/routes/sparkpoints/index.js
@@ -9,34 +9,69 @@ function *getHandler() {
     ctx.assert(sectionId > 0, 'section_id is required', 400);
 
     ctx.body = yield ctx.pgp.any(/*language=SQL*/ `
-       SELECT sp.id,
-              sp.code AS sparkpoint,
-              -- TODO: check with @rgipson to see when we can remove code
-              sp.code,
+       -- noinspection JSLint
+       SELECT sp.code AS sparkpoint_code,
               sp.student_title,
-              
+                            
               cs."Code" AS section,
-              
-              ss.*,
               
               ssas.last_accessed,
               ssas.section_id,
+              ssas.student_id,
+              ssas.sparkpoint_id,
               ssas.recommender_id,
-              ssas.recommended_time
+              ssas.recommended_time,
+               
+              learn_start_time,
+              learn_finish_time,
+              learn_override_teacher_id,
+              learn_override_time,
+               
+              conference_start_time,
+              conference_finish_time,
+              conference_join_time,
+              conference_override_teacher_id,
+              conference_override_time,
+               
+              apply_start_time,
+              apply_ready_time,
+              apply_finish_time,
+              apply_override_teacher_id,
+              apply_override_time,
+               
+              assess_start_time,
+              assess_ready_time,
+              assess_finish_time,
+              assess_override_teacher_id,
+              assess_override_time,
+               
+              override_reason,
+               
+              conference_group_id,
+              selected_apply_id,
+              selected_apply_resource_id,
+              learn_mastery_check_score,
+              conference_mastery_check_score,
+               
+              learn_pace_target,
+              conference_pace_target,
+              apply_pace_target,
+              assess_pace_target
+              
          FROM section_student_active_sparkpoint ssas
     LEFT JOIN student_sparkpoint ss ON ss.sparkpoint_id = ssas.sparkpoint_id
           AND ss.student_id = ssas.student_id
          JOIN sparkpoints sp ON sp.id = ssas.sparkpoint_id
          JOIN course_sections cs ON cs."ID" = ssas.section_id
         WHERE ssas.section_id = $1
-          ${studentWhere}
+          ${          studentWhere}
      ORDER BY ssas.last_accessed DESC
     `, [
         sectionId,
-        studentId
+        studentId,
     ]);
 }
 
 module.exports = {
-    get: getHandler
+    get: getHandler,
 };
