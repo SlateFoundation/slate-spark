5fed3a2488489f96ee83cca0ad2e7bb4aed102ed
Ryon Coleman <ryon55@gmail.com>
2016-10-21 12:14:39 -0400

    Add new sparkpoint/phase selects and timer to app titlebar

--- sencha-workspace/packages/spark-classroom/src/TitleBar.js
+++ sencha-workspace/packages/spark-classroom/src/TitleBar.js
@@ -3,7 +3,8 @@ Ext.define('SparkClassroom.TitleBar', {
     extend: 'Ext.Toolbar',
     xtype: 'spark-titlebar',
     requires: [
-        'Ext.field.Select'
+        'Ext.field.Select',
+        'SparkClassroom.work.Timer'
     ],
 
     config: {
@@ -30,6 +31,55 @@ Ext.define('SparkClassroom.TitleBar', {
                     cls: 'spark-course-picker',
                     width: null
                 }
+            },
+            {
+                xtype: 'spacer'
+            },
+            {
+                xtype: 'container',
+                items: [
+                    {
+                        xtype: 'component',
+                        cls: 'spark-gps-selection-status',
+                        tpl: '{n} student<tpl if="n != 1">s</tpl> selected. &ensp; <a href="#">Deselect All</a>',
+                        data: { n: 0 }
+                    },
+                    {
+                        xtype: 'container',
+                        layout: 'hbox',
+                        defaults: {
+                            autoSelect: false,
+                            margin: '0 16 0 0'
+                        },
+                        items: [
+                            {
+                                xtype: 'selectfield',
+                                placeHolder: 'Select Sparkpoint',
+                                options: [
+                                    { text: '[Use Sparkpoint picker here]' },
+                                    { text: '[Examples:]' },
+                                    { text: 'MATH.G6.G.1' },
+                                    { text: 'MATH.H6.H.3' },
+                                    { text: 'SCIENCE.J5.K.1' }
+                                ]
+                            },
+                            {
+                                xtype: 'selectfield',
+                                placeHolder: 'Move Phase',
+                                options: [
+                                    { text: 'Learn and Practice' },
+                                    { text: 'Conference' },
+                                    { text: 'Apply' },
+                                    { text: 'Assess' }
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                xtype: 'spark-work-timer',
+                margin: '24 0 0'
             }
         ]
     }
