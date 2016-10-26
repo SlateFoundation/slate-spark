1a5794dd2b3ed942e02ebb2f265b3d87c78c39e6
Ryon Coleman <ryon55@gmail.com>
2016-10-26 05:58:11 -0400

    Move and restyle K-1 features from app titlebar to new GPS toolbar

--- sencha-workspace/packages/spark-classroom/src/TitleBar.js
+++ sencha-workspace/packages/spark-classroom/src/TitleBar.js
@@ -36,60 +36,7 @@ Ext.define('SparkClassroom.TitleBar', {
                 xtype: 'spacer'
             },
             {
-                xtype: 'container',
-                items: [
-                    {
-                        xtype: 'checkboxfield',
-                        cls: 'spark-toggle-student-multiselect',
-                        name: 'spark-toggle-student-multiselect',
-                        labelAlign: 'left',
-                        width: 130,
-                        labelWidth: 130,
-                        label: 'Student Multiselect' // TODO: This is for testing student multiselect, and should be removed as it is a placeholder.
-                    },
-                    {
-                        xtype: 'component',
-                        cls: 'spark-gps-selection-status',
-                        tpl: '{n} student<tpl if="n != 1">s</tpl> selected. &ensp; <a href="#">Deselect All</a>',
-                        data: { n: 0 }
-                    },
-                    {
-                        xtype: 'container',
-                        layout: 'hbox',
-                        cls: 'spark-gps-selection-operations',
-                        defaults: {
-                            autoSelect: false,
-                            margin: '0 16 0 0'
-                        },
-                        items: [
-                            {
-                                xtype: 'selectfield',
-                                placeHolder: 'Select Sparkpoint',
-                                options: [
-                                    { text: '[Use Sparkpoint picker here]' },
-                                    { text: '[Examples:]' },
-                                    { text: 'MATH.G6.G.1' },
-                                    { text: 'MATH.H6.H.3' },
-                                    { text: 'SCIENCE.J5.K.1' }
-                                ]
-                            },
-                            {
-                                xtype: 'selectfield',
-                                placeHolder: 'Move Phase',
-                                options: [
-                                    { text: 'Learn and Practice' },
-                                    { text: 'Conference' },
-                                    { text: 'Apply' },
-                                    { text: 'Assess' }
-                                ]
-                            }
-                        ]
-                    }
-                ]
-            },
-            {
-                xtype: 'spark-work-timer',
-                margin: '24 0 0'
+                xtype: 'spark-work-timer'
             }
         ]
     }
