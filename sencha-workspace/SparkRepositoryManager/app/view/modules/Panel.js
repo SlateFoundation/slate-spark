4f1f1a7d1c300c8df6ba4cebeebc43e207f8b697
Bill Clinton <BillClinton@users.noreply.github.com>
2016-10-27 19:57:16 -0400

    Add module config item for current module record

--- sencha-workspace/SparkRepositoryManager/app/view/modules/Panel.js
+++ sencha-workspace/SparkRepositoryManager/app/view/modules/Panel.js
@@ -1,10 +1,23 @@
+/**
+ * The top level container for the Modules section of the website
+ *
+ * @event moduleupdate
+ * Fires when the module has been updated.
+ * @param {SparkRepositoryManager.view.modules.Panel} moduleContainer
+ * @param {SparkRepositoryManager.model.Module} module
+ * @param {SparkRepositoryManager.model.Module} oldModule
+ */
 Ext.define('SparkRepositoryManager.view.modules.Panel', {
     extend: 'Ext.panel.Panel',
+    xtype: 's2m-modules-panel',
     requires: [
         'SparkRepositoryManager.view.modules.Navigator',
         'SparkRepositoryManager.view.modules.editor.Editor'
     ],
-    xtype: 's2m-modules-panel',
+
+    config: {
+        module: null
+    },
 
     layout: 'border',
 
@@ -18,5 +31,12 @@ Ext.define('SparkRepositoryManager.view.modules.Panel', {
             region: 'center',
             xtype: 's2m-modules-editor'
         }
-    ]
+    ],
+
+    updateModule: function(module, oldModule) {
+        var me = this;
+
+        me.fireEvent('moduleupdate', me, module, oldModule);
+    }
+
 });
