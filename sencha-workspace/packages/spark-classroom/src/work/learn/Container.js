2d38b12206084085ec3d79cc778a7e3c1c70ca76
Ryon Coleman <ryon55@gmail.com>
2016-11-03 07:15:20 -0400

    Add module info box and example group accordion item to Learn UI

--- sencha-workspace/packages/spark-classroom/src/work/learn/Container.js
+++ sencha-workspace/packages/spark-classroom/src/work/learn/Container.js
@@ -13,8 +13,48 @@ Ext.define('SparkClassroom.work.learn.Container', {
         allowToggleComplete: true,
 
         items: [{
+            // TODO wire up this example module info box
+
+            xtype: 'component',
+            cls: 'spark-panel spark-work-learn-module-intro',
+            data: {
+                moduleTitle: 'Module Title <small>Module Code</small>',
+                moduleDescription: 'This is a description or intro to the module, written by the instructor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer accumsan libero purus, eget tincidunt nisi sagittis aliquet.'
+            },
+            tpl: [
+                '<h3 class="spark-panel-title">{moduleTitle}</h3>',
+                '<p>{moduleDescription}</p>'
+            ]
+        },{
             layout: 'accordion',
             items: [
+                {
+                    // TODO example module learn group
+                    // just add another of these items for each group
+
+                    expanded: true,
+
+                    xtype: 'container',
+                    title: 'Module Group Name <small>Module Code</small>', // TODO if no group name available, number the groups sequentially: 'Group 1', 'Group 2'
+                    items: [
+                        {
+                            xtype: 'container',
+                            layout: {
+                                type: 'hbox',
+                                pack: 'center'
+                            },
+                            items: [
+                                {
+                                    xtype: 'spark-work-learn-progressbanner',
+                                    hidden: true
+                                }
+                            ]
+                        },
+                        {
+                            xtype: 'spark-work-learn-grid'
+                        }
+                    ]
+                },
                 {
                     expanded: true,
                     itemId: 'sparkpointCt',
