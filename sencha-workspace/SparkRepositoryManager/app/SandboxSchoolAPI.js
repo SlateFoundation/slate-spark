3c4972a9e3d8d0eac93f451cd7b07d753a6197ad
Bill Clinton <BillClinton@users.noreply.github.com>
2016-11-03 04:40:41 -0400

    Add temp custom proxy/connection for sandboxschool

---
+++ sencha-workspace/SparkRepositoryManager/app/SandboxSchoolAPI.js
@@ -0,0 +1,8 @@
+// TODO: delete me - this is a dev workaround to contact a different API host
+Ext.define('SparkRepositoryManager.SandboxSchoolAPI', {
+    extend: 'Emergence.util.AbstractAPI',
+    singleton: true,
+
+    host: 'sandbox-school.matchbooklearning.com',
+    useSSL: true
+});
---
+++ sencha-workspace/SparkRepositoryManager/app/proxy/SandboxSchool.js
@@ -0,0 +1,8 @@
+// TODO: delete me - this is a dev workaround to contact a different API host
+Ext.define('SparkRepositoryManager.proxy.SandboxSchool', {
+    extend: 'Jarvus.proxy.API',
+    alias: 'proxy.sandbox-school',
+
+    connection: 'SparkRepositoryManager.SandboxSchoolAPI'
+
+});
