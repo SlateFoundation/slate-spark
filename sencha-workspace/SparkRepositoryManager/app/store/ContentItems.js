2a8ed64716c8449b13e6d289d95d3c4bd100171f
Bill Clinton <BillClinton@users.noreply.github.com>
2016-11-04 15:47:47 -0400

    Duplicate recs to appear in multiple groups

--- sencha-workspace/SparkRepositoryManager/app/store/ContentItems.js
+++ sencha-workspace/SparkRepositoryManager/app/store/ContentItems.js
@@ -7,11 +7,55 @@ Ext.define('SparkRepositoryManager.store.ContentItems', {
 
     model: 'SparkRepositoryManager.model.ContentItem',
 
+    groupField: 'sparkpointGroup',
+
+    proxy: {
+        // TODO: this proxy is a dev workaround to contact a different API host
+        type: 'sandbox-school',
+        url: '/spark/api/work/resources',
+        reader: {
+            type: 'json'
+        }
+    },
+
     listeners: {
-        'beforeload': function(store) {
-            console.log('beforeload');
-            console.log(store.getModel().getProxy().getConnection());
-//            store.getModel().getProxy().getConnection().setHost('sandbox-school.matchbooklearning.com');
+        'load': function(store, records) {
+            if (store.count()>0 && store.lastOptions && store.lastOptions.params && store.lastOptions.params.sparkpoint_ids) {
+                this.setGroupFieldValue(records, store.lastOptions.params.sparkpoint_ids.split(','));
+            }
+        }
+    },
+
+    setGroupFieldValue: function(records, sparkpointIds) {
+        var recordsLength = records.length,
+            sparkpointIdsLength = sparkpointIds.length,
+            sparkpointId,
+            sparkpointIndex,
+            recordSparkpointIds,
+            rec,
+            newRec,
+            i = 0,
+            r;
+
+        for (; i < sparkpointIdsLength; i++) {
+            sparkpointId = sparkpointIds[i];
+
+            for (r=0; r < recordsLength; r++) {
+                rec = records[r];
+
+                recordSparkpointIds = records[r].get('sparkpoint_ids');
+                sparkpointIndex = recordSparkpointIds.indexOf(sparkpointId);
+
+                if (sparkpointIndex >= 0) {
+                    if (rec.get('sparkpointGroup')) {
+                        newRec = rec.copy(null);
+                        newRec.set('sparkpointGroup', rec.get('sparkpoints')[sparkpointIndex]);
+                        this.add(newRec);
+                    } else {
+                        rec.set('sparkpointGroup', rec.get('sparkpoints')[sparkpointIndex]);
+                    }
+                }
+            }
         }
     }
 
