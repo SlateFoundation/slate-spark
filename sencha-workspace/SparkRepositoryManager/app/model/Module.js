4f97544ca1cca968f79ea8f16a09ac9b8e68da3a
Bill Clinton <BillClinton@users.noreply.github.com>
2016-11-09 09:55:26 -0500

    Update model fields and proxy

--- sencha-workspace/SparkRepositoryManager/app/model/Module.js
+++ sencha-workspace/SparkRepositoryManager/app/model/Module.js
@@ -1,75 +1,155 @@
 Ext.define('SparkRepositoryManager.model.Module', {
     extend: 'Ext.data.Model',
     requires: [
-        'Ext.data.proxy.LocalStorage'
+        'SparkRepositoryManager.model.ModuleData'
     ],
 
     proxy: {
-        type: 'localstorage',
-        id: 'module-dev'
+        // TODO: this proxy is a dev workaround to contact a different API host
+        type: 'sandbox-school',
+        url: '/spark/api/work/modules',
+        reader: {
+            type: 'json'
+        }
     },
+
+    identifier: 'negative',
+//    idProperty: 'dummyId',
 
     fields: [
         {
-            name: 'id'
+            name: 'id',
+            type: 'auto',
+            allowNull: true
         },
         {
-            name: 'title',
-            type: 'string'
+            name: 'dummyId',
+            type: 'auto',
+            persist: false,
+            allowNull: true
         },
         {
             name: 'code',
             type: 'string',
-            defaultValue: 'DRAFT'
-        },
-        {
-            name: 'contentAreaId',
-            mapping: 'content_area_id',
-            type: 'int',
-            allowNull: true,
-            defaultValue: null
+            persist: false,
+            allowNull: true
         },
         {
             name: 'author_id',
-            type: 'int'
+            type: 'int',
+            critical: true,
+            allowNull: true
+        },
+        {
+            name: 'created',
+            type: 'date',
+            dateFormat: 'timestamp',
+            critical: true,
+            allowNull: true
+        },
+        {
+            name: 'modified',
+            type: 'date',
+            dateFormat: 'timestamp',
+            critical: true,
+            allowNull: true
         },
         {
             name: 'published',
-            type: 'bool'
+            type: 'date',
+            // dateFormat: 'timestamp',
+            dateFormat: 'c',
+            critical: true,
+            allowNull: true
         },
+
+/*
+        {
+            name: 'template',
+            type: 'string',
+            allowNull: true,
+            convert: function(val, rec) {
+                return Ext.encode(rec.get('moduleData'));
+            }
+        },
+        {
+            name: 'moduleData',
+            type: 'auto',
+            defaultValue: {},
+            persist: false
+        },
+*/
+
         {
             name: 'global',
+            critical: true,
             type: 'bool'
         },
         {
-            name: 'prompt',
-            type: 'string'
+            name: 'grade',
+            type: 'string',
+            critical: true,
+            allowNull: true
+        },
+        {
+            name: 'content_area',
+            type: 'int',
+            critical: true,
+            allowNull: true
+        },
+        {
+            name: 'title',
+            type: 'string',
+            critical: true
         },
         {
             name: 'sparkpoints',
             type: 'auto',
-            default: []
+            critical: true,
+            default: 'yo'
         },
         {
             name: 'learns',
             type: 'auto',
+            critical: true,
             default: []
         },
         {
             name: 'conference_questions',
             type: 'auto',
+            critical: true,
             default: []
         },
         {
             name: 'conference_resources',
             type: 'auto',
+            critical: true,
             default: []
         },
         {
             name: 'applies',
             type: 'auto',
+            critical: true,
             default: []
+        },
+
+        // Define tree fields so persist can be set to false
+        {
+            name: 'leaf',
+            persist: false
+        },
+        {
+            name: 'prompt',
+            persist: false
+        },
+        {
+            name: 'parentId',
+            persist: false,
+            convert: function(val, rec) {
+                return rec.get('global') === true ? 'Global' : 'Personal';
+            }
         }
+
     ]
 
 });
