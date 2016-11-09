a8dc4e20d26f6a75c2256bfc1a1045841cd47e30
Bill Clinton <BillClinton@users.noreply.github.com>
2016-11-09 12:56:14 -0500

    Override API actions to match API

--- sencha-workspace/SparkRepositoryManager/app/proxy/SandboxSchool.js
+++ sencha-workspace/SparkRepositoryManager/app/proxy/SandboxSchool.js
@@ -1,8 +1,22 @@
-// TODO: delete me - this is a dev workaround to contact a different API host
+// TODO: this is a dev workaround to contact a different API host
 Ext.define('SparkRepositoryManager.proxy.SandboxSchool', {
     extend: 'Jarvus.proxy.API',
     alias: 'proxy.sandbox-school',
 
-    connection: 'SparkRepositoryManager.SandboxSchoolAPI'
+    connection: 'SparkRepositoryManager.SandboxSchoolAPI',
 
+    getMethod: function(request) {
+        switch (request.getAction()) {
+            case 'create':
+                return 'PATCH';
+            case 'read':
+                return 'GET';
+            case 'update':
+                return 'PATCH';
+            case 'destroy':
+                return 'DELETE';
+            default:
+                return 'GET';
+        }
+    }
 });
