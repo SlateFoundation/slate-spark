18fdf2350875970202e1f080648db328940e1656
Bill Clinton <BillClinton@users.noreply.github.com>
2016-11-09 09:57:09 -0500

    Alter comment

--- sencha-workspace/SparkRepositoryManager/app/SandboxSchoolAPI.js
+++ sencha-workspace/SparkRepositoryManager/app/SandboxSchoolAPI.js
@@ -1,8 +1,9 @@
-// TODO: delete me - this is a dev workaround to contact a different API host
+// TODO: this is a dev workaround to contact a different API host
 Ext.define('SparkRepositoryManager.SandboxSchoolAPI', {
     extend: 'Emergence.util.AbstractAPI',
     singleton: true,
 
     host: 'sandbox-school.matchbooklearning.com',
     useSSL: true
+
 });
