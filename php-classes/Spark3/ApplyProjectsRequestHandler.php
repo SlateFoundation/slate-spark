2494e27913a36be05ea1f9aa37c47a3723a478a9
Christopher Alfano <calfano@matchbooklearning.com>
2017-01-02 23:25:45 -0500

    Remove alternate access control used by stopgap projects screen

--- php-classes/Spark3/ApplyProjectsRequestHandler.php
+++ php-classes/Spark3/ApplyProjectsRequestHandler.php
@@ -4,24 +4,5 @@ namespace Spark3;
 
 class ApplyProjectsRequestHandler extends \Emergence\ActiveRecord\RequestHandler
 {
-    public static $anonymousOrigins = [];
-
     public static $recordClass = ApplyProject::class;
-    public static $accountLevelRead = 'User';
-
-    public static function checkBrowseAccess($browseArguments)
-    {
-        return (
-            in_array($_SERVER['HTTP_ORIGIN'], static::$anonymousOrigins) ||
-            parent::checkBrowseAccess($browseArguments)
-        );
-    }
-
-    public static function checkReadAccess(\Emergence\ActiveRecord\ActiveRecordInterface $Record, $suppressLogin = false)
-    {
-        return (
-            in_array($_SERVER['HTTP_ORIGIN'], static::$anonymousOrigins) ||
-            parent::checkReadAccess($Record, $suppressLogin)
-        );
-    }
 }
